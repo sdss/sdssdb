b9904be509022df0884d5fa5b867e7b723d4e8b7
Conor Sayres <csayres@uw.edu>
2018-09-24 15:42:19 -0700

    twomass clean query.

---
+++ schema/catalogdb/functions/twoMassClean.sql
@@ -0,0 +1,16 @@
+\timing
+SELECT pts_key, designation, ra, decl, h_m catalogdb.twomass_clean FROM catalogdb.twomass_psc
+    WHERE h_m < 11
+    and (ph_qual like '_A_' or ph_qual like '_B_')
+    and cc_flg like '_0_'
+    and gal_contam = '0' and
+    and (rd_flg like '_1_' or rd_flg like '_2_');
+
+alter table catalogdb.twomass_clean add primary key(designation);
+ALTER TABLE catalogdb.twomass_clean ADD CONSTRAINT twomass_psc_desig_unique UNIQUE (designation);
+CREATE INDEX CONCURRENTLY ON catalogdb.twomass_clean using BTREE (h_m);
+create index on catalogdb.twomass_clean (q3c_ang2ipix(ra, decl));
+CLUSTER twomass_clean_q3c_ang2ipix_idx on catalogdb.twomass_clean;
+analyze catalogdb.twomass_clean;
+
+\timing
