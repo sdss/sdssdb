c0e2ed865d4f5484e52c9008007dd0f2863c5819
Conor Sayres <csayres@uw.edu>
2018-09-24 15:21:18 -0700

    use limit not top

--- schema/catalogdb/functions/gaiaClean.sql
+++ schema/catalogdb/functions/gaiaClean.sql
@@ -1,4 +1,4 @@
-SELECT TOP 5 phot_g_mean_mag+5*log10(parallax)-10 AS mg, bp_rp FROM catalogdb.gaia_dr2_source
+SELECT phot_g_mean_mag+5*log10(parallax)-10 AS mg, bp_rp FROM catalogdb.gaia_dr2_source
    WHERE parallax_over_error > 10
    AND phot_g_mean_flux_over_error>50
    AND phot_rp_mean_flux_over_error>20
@@ -7,3 +7,4 @@ SELECT TOP 5 phot_g_mean_mag+5*log10(parallax)-10 AS mg, bp_rp FROM catalogdb.ga
    AND phot_bp_rp_excess_factor > 1.0+0.015*power(phot_bp_mean_mag-phot_rp_mean_mag,2)
    AND visibility_periods_used>8
    AND astrometric_chi2_al/(astrometric_n_good_obs_al-5)<1.44*greatest(1,exp(-0.4*(phot_g_mean_mag-19.5)))
+   limit 10
