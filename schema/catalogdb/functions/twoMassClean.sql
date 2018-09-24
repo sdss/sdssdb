325f108c936a724f07ac561438923f22d18e1a80
Conor Sayres <csayres@uw.edu>
2018-09-24 15:47:56 -0700

    remove index stuff etc

--- schema/catalogdb/functions/twoMassClean.sql
+++ schema/catalogdb/functions/twoMassClean.sql
@@ -6,11 +6,11 @@ SELECT pts_key, designation, ra, decl, h_m into catalogdb.twomass_clean FROM cat
     and gal_contam = '0' and
     and (rd_flg like '_1_' or rd_flg like '_2_');
 
-alter table catalogdb.twomass_clean add primary key(designation);
-ALTER TABLE catalogdb.twomass_clean ADD CONSTRAINT twomass_psc_desig_unique UNIQUE (designation);
-CREATE INDEX CONCURRENTLY ON catalogdb.twomass_clean using BTREE (h_m);
-create index on catalogdb.twomass_clean (q3c_ang2ipix(ra, decl));
-CLUSTER twomass_clean_q3c_ang2ipix_idx on catalogdb.twomass_clean;
-analyze catalogdb.twomass_clean;
+-- alter table catalogdb.twomass_clean add primary key(designation);
+-- ALTER TABLE catalogdb.twomass_clean ADD CONSTRAINT twomass_psc_desig_unique UNIQUE (designation);
+-- CREATE INDEX CONCURRENTLY ON catalogdb.twomass_clean using BTREE (h_m);
+-- create index on catalogdb.twomass_clean (q3c_ang2ipix(ra, decl));
+-- CLUSTER twomass_clean_q3c_ang2ipix_idx on catalogdb.twomass_clean;
+-- analyze catalogdb.twomass_clean;
 
 \timing
