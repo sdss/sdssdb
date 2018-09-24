96643da25fdc76e7a3e175e7cece38dfd612f32f
Conor Sayres <csayres@uw.edu>
2018-09-24 15:31:51 -0700

    try full query

--- schema/catalogdb/functions/gaiaClean.sql
+++ schema/catalogdb/functions/gaiaClean.sql
@@ -7,6 +7,6 @@ SELECT source_id into catalogdb.gaia_dr2_clean FROM catalogdb.gaia_dr2_source
    AND phot_bp_rp_excess_factor < 1.3+0.06*power(phot_bp_mean_mag-phot_rp_mean_mag,2)
    AND phot_bp_rp_excess_factor > 1.0+0.015*power(phot_bp_mean_mag-phot_rp_mean_mag,2)
    AND visibility_periods_used>8
-   AND astrometric_chi2_al/(astrometric_n_good_obs_al-5)<1.44*greatest(1,exp(-0.4*(phot_g_mean_mag-19.5)))
-   limit 10;
+   AND astrometric_chi2_al/(astrometric_n_good_obs_al-5)<1.44*greatest(1,exp(-0.4*(phot_g_mean_mag-19.5)));
+alter table catalogdb.gaia_dr2_clean add primary key(source_id);
 \timing
