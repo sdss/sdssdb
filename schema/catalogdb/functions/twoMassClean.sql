a6e3de1943c05ed082276c65ff5305f2efc39a76
Conor Sayres <u0449727@sdssadmin.wasatch.peaks>
2018-09-24 17:23:50 -0600

    muddlin

--- schema/catalogdb/functions/twoMassClean.sql
+++ schema/catalogdb/functions/twoMassClean.sql
@@ -3,14 +3,14 @@ SELECT pts_key, designation, ra, decl, h_m into catalogdb.twomass_clean FROM cat
     WHERE h_m < 11
     and (ph_qual like '_A_' or ph_qual like '_B_')
     and cc_flg like '_0_'
-    and gal_contam = '0' and
+    and gal_contam = '0'
     and (rd_flg like '_1_' or rd_flg like '_2_');
 
--- alter table catalogdb.twomass_clean add primary key(designation);
--- ALTER TABLE catalogdb.twomass_clean ADD CONSTRAINT twomass_psc_desig_unique UNIQUE (designation);
--- CREATE INDEX CONCURRENTLY ON catalogdb.twomass_clean using BTREE (h_m);
--- create index on catalogdb.twomass_clean (q3c_ang2ipix(ra, decl));
--- CLUSTER twomass_clean_q3c_ang2ipix_idx on catalogdb.twomass_clean;
--- analyze catalogdb.twomass_clean;
+alter table catalogdb.twomass_clean add primary key(designation);
+ALTER TABLE catalogdb.twomass_clean ADD CONSTRAINT twomass_clean_desig_unique UNIQUE (designation);
+CREATE INDEX CONCURRENTLY ON catalogdb.twomass_clean using BTREE (h_m);
+create index concurrently on catalogdb.twomass_clean (q3c_ang2ipix(ra, decl));
+CLUSTER twomass_clean_q3c_ang2ipix_idx on catalogdb.twomass_clean;
+analyze catalogdb.twomass_clean;
 
 \timing
