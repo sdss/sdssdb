bb1f1e157a3ddfb2dba16130e0cb3f63a3ce8df5
Conor Sayres <csayres@uw.edu>
2018-09-27 18:49:46 -0700

    try2

--- schema/catalogdb/functions/galacticGenesis.sql
+++ schema/catalogdb/functions/galacticGenesis.sql
@@ -1,8 +1,8 @@
 \timing
 select gaia_dr2_clean.source_id, gaia_dr2_clean.ra, gaia_dr2_clean.dec, gaia_dr2_clean.phot_g_mean_mag, twomass_clean.h_m
-from gaia_dr2_clean, twomass_clean, gaiadr2_tmass_best_neighbor
-inner join gaia_dr2_clean.source_id on gaiadr2_tmass_best_neighbor.source_id
-inner join gaiadr2_tmass_best_neighbor.original_ext_source_id on twomass_clean.designation
+from catalogdb.gaia_dr2_clean, catalogdb.twomass_clean
+inner join catalogdb.gaia_dr2_clean.source_id on catalogdb.gaiadr2_tmass_best_neighbor.source_id
+inner join catalogdb.gaiadr2_tmass_best_neighbor.original_ext_source_id on catalogdb.twomass_clean.designation
 limit 10;
 
 \timing
