263a4446570489d6f2c470e57fe76be398097e84
Conor Sayres <csayres@uw.edu>
2018-09-28 10:42:40 -0700

    into table

--- schema/catalogdb/functions/galacticGenesis.sql
+++ schema/catalogdb/functions/galacticGenesis.sql
@@ -1,6 +1,7 @@
 \timing
 
 select a.source_id as gaiaid, b.original_ext_source_id as twomassid, c.ra as g_ra, c.dec as g_dec, d.ra as t_ra, d.decl as t_dec, c.phot_g_mean_mag, d.h_m
+into catalogdb.galactic_genesis
 from catalogdb.gaia_dr2_clean a
 inner join catalogdb.gaiadr2_tmass_best_neighbour b on b.source_id = a.source_id
 inner join catalogdb.gaia_dr2_source c on c.source_id = a.source_id
