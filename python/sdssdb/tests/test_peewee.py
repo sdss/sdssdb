34847d700add20499eb9f33e08b95da7e6d972a4
Michael Blanton <u0919333@manga.wasatch.peaks>
2018-10-26 14:19:14 -0600

    test for peewee

---
+++ python/sdssdb/tests/test_peewee.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+#
+# main.py
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import sdssdb.peewee.targetdb as targetdb
+
+
+class TestPeewee(object):
+    """Tests peewee db utils"""
+
+    def test_connection(self):
+        """Tests peewee db connection"""
+
+        nt = targetdb.Target.select().count()
+        assert nt > 0
