368005c8cbe9c10c7930ee6eef2e1d0cc4a71933
José Sánchez-Gallego <gallegoj@uw.edu>
2018-12-14 22:44:13 -0800

    Bump version: 0.2.1 → 0.2.2dev

--- python/sdssdb/__init__.py
+++ python/sdssdb/__init__.py
@@ -43,7 +43,7 @@ warnings.filterwarnings(
     'ignore', '.*Skipped unsupported reflection of expression-based index .*q3c.*')
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2dev'
 
 
 from .connection import *  # noqa
