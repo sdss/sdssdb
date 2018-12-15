96f4ce1e9fd9e2dd0da526c6ff0f1ab388d6074f
José Sánchez-Gallego <gallegoj@uw.edu>
2018-12-14 22:23:19 -0800

    Bump version: 0.2.1dev → 0.2.1

--- python/sdssdb/__init__.py
+++ python/sdssdb/__init__.py
@@ -43,7 +43,7 @@ warnings.filterwarnings(
     'ignore', '.*Skipped unsupported reflection of expression-based index .*q3c.*')
 
 
-__version__ = '0.2.1dev'
+__version__ = '0.2.1'
 
 
 from .connection import *  # noqa
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sys
 # The NAME variable should be of the format "sdss-sdssdb".
 # Please check your NAME adheres to that format.
 NAME = 'sdssdb'
-VERSION = '0.2.1dev'
+VERSION = '0.2.1'
 RELEASE = 'dev' in VERSION
 
 
