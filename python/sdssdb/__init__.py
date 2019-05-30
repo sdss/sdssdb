b7a7d6d055f14ea09e79e76071ee1b89ec3596e7
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-30 06:58:54 -0600

    Support for different versions of PYYAML

--- python/sdssdb/__init__.py
+++ python/sdssdb/__init__.py
@@ -4,8 +4,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 import warnings
+from pkg_resources import parse_version
 
 import yaml
+
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(name).
@@ -28,14 +30,19 @@ def merge(user, default):
 NAME = 'sdssdb'
 
 
+yaml_kwds = dict()
+if parse_version(yaml.__version__) >= parse_version('5.1'):
+    yaml_kwds.update(Loader=yaml.FullLoader)
+
+
 # Loads config
 config_file = os.path.dirname(__file__) + '/etc/{0}.yml'.format(NAME)
-config = yaml.load(open(config_file), Loader=yaml.FullLoader)
+config = yaml.load(open(config_file), **yaml_kwds)
 
 # If there is a custom configuration file, updates the defaults using it.
 custom_config_fn = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
 if os.path.exists(custom_config_fn):
-    config = merge(yaml.load(open(custom_config_fn), Loader=yaml.FullLoader), config)
+    config = merge(yaml.load(open(custom_config_fn), **yaml_kwds), config)
 
 # from sdssdb.sqlalchemy import db
 # config['db'] = db
