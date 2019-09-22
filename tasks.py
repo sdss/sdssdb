eaaa60e928625a130917649e4c59b7af59c7d85f
José Sánchez-Gallego <gallegoj@uw.edu>
2019-09-21 21:12:50 -0700

    Update build framework (#17)
    * Remove files
    
    * Add deploy to test PyPI task
    
    * Use setup.cfg
    
    * Update readthedocs
    
    * Update changelog
    
    * Some changes to setup.cfg

--- tasks.py
+++ tasks.py
@@ -24,7 +24,7 @@ from invoke import Collection, task
 def clean_docs(ctx):
     """Cleans up the docs"""
     print('Cleaning the docs')
-    ctx.run("rm -rf docs/sphinx/_build")
+    ctx.run('rm -rf docs/sphinx/_build')
 
 
 @task
@@ -33,11 +33,11 @@ def build_docs(ctx, clean=False):
 
     if clean:
         print('Cleaning the docs')
-        ctx.run("rm -rf docs/sphinx/_build")
+        ctx.run('rm -rf docs/sphinx/_build')
 
     print('Building the docs')
     os.chdir('docs/sphinx')
-    ctx.run("make html", pty=True)
+    ctx.run('make html', pty=True)
 
 
 @task
@@ -60,18 +60,26 @@ def clean(ctx):
     ctx.run('rm -rf python/*.egg-info')
 
 
-@task(clean)
+@task(clean, default=True)
 def deploy(ctx):
-    """Deploy the project to pypi"""
-    print('Deploying to Pypi!')
+    """Deploy the project to PyPI"""
+    print('Deploying to PyPI!')
     ctx.run('python setup.py sdist bdist_wheel --universal')
     ctx.run('twine upload dist/*')
+
+
+@task(clean)
+def deploy_test(ctx):
+    """Deploy the project to the test version of  PyPI"""
+    print('Deploying to Test PyPI!')
+    ctx.run('python setup.py sdist bdist_wheel --universal')
+    ctx.run('twine upload --repository-url https://test.pypi.org/legacy/ dist/*')
 
 
 os.chdir(os.path.dirname(__file__))
 
 # create a collection of tasks
-ns = Collection(clean, deploy)
+ns = Collection(clean)
 
 # create a sub-collection for the doc tasks
 docs = Collection('docs')
@@ -79,3 +87,8 @@ docs.add_task(build_docs, 'build')
 docs.add_task(clean_docs, 'clean')
 docs.add_task(show_docs, 'show')
 ns.add_collection(docs)
+
+deploy_task = Collection('deploy')
+deploy_task.add_task(deploy, 'pypi')
+deploy_task.add_task(deploy_test, 'test')
+ns.add_collection(deploy_task)
