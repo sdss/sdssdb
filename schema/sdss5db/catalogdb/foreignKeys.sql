73f76c617d21990bcf12b95576e5c3553376d7a7
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-11 20:45:15 -0700

    More clear structure for foreign keys

--- schema/sdss5db/catalogdb/foreignKeys.sql
+++ schema/sdss5db/catalogdb/foreignKeys.sql
@@ -9,11 +9,32 @@ https://www.postgresql.org/docs/8.2/static/sql-altertable.html
 
 */
 
-ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour ADD CONSTRAINT original_ext_source_id_fk FOREIGN KEY (original_ext_source_id) REFERENCES catalogdb.twomass_psc (designation) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
+    ADD CONSTRAINT original_ext_source_id_fk
+    FOREIGN KEY (original_ext_source_id)
+    REFERENCES catalogdb.twomass_psc (designation)
+    ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE catalogdb.gaia_dr2_wd_candidates_v1 ADD CONSTRAINT source_id_fk FOREIGN KEY (source_id) REFERENCES catalogdb.gaia_dr2_source (source_id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE catalogdb.gaia_dr2_wd_candidates_v1
+    ADD CONSTRAINT source_id_fk
+    FOREIGN KEY (source_id)
+    REFERENCES catalogdb.gaia_dr2_source (source_id)
+    ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour ADD CONSTRAINT source_id_fk FOREIGN KEY (source_id) REFERENCES catalogdb.gaia_dr2_source (source_id) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour ADD CONSTRAINT tmass_pts_key_fk FOREIGN KEY (tmass_pts_key) REFERENCES catalogdb.twomass_psc (pts_key) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
+    ADD CONSTRAINT source_id_fk
+    FOREIGN KEY (source_id)
+    REFERENCES catalogdb.gaia_dr2_source (source_id)
+    ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE catalogdb.sdss_dr14_specobj ADD CONSTRAINT bestobjid_fk FOREIGN KEY (bestobjid) REFERENCES catalogdb.sdss_dr13_photoobj (objid) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
+    ADD CONSTRAINT tmass_pts_key_fk
+    FOREIGN KEY (tmass_pts_key)
+    REFERENCES catalogdb.twomass_psc (pts_key)
+    ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE catalogdb.sdss_dr14_specobj
+    ADD CONSTRAINT bestobjid_fk
+    FOREIGN KEY (bestobjid)
+    REFERENCES catalogdb.sdss_dr13_photoobj (objid)
+    ON UPDATE CASCADE ON DELETE CASCADE;
