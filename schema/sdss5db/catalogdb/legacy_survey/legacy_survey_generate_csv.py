f3ea634567b8459dee6de78b9efc9347c6077dc7
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-13 13:37:33 -0700

    Updated scripts for legacy survey

---
+++ schema/sdss5db/catalogdb/legacy_survey/legacy_survey_generate_csv.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2020-03-12
+# @Filename: legacy_survey_load.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import glob
+import multiprocessing
+
+import astropy.table
+import progressbar
+
+from sdssdb.peewee.sdss5db import database
+from sdssdb.utils.ingest import to_csv
+
+
+assert database.connected, 'database is not connected'
+
+
+def convert_to_csv(file_):
+
+    data = astropy.table.Table.read(file_)
+    data.meta = {}
+    data.rename_columns(data.colnames, list(map(lambda x: x.lower(), data.colnames)))
+    to_csv(data, file_ + '.csv', header=True, convert_arrays=True, overwrite=True)
+
+
+if __name__ == '__main__':
+
+    files = glob.glob('sweep*.fits')
+
+    pool = multiprocessing.Pool(1)
+
+    for _ in progressbar.progressbar(pool.imap_unordered(convert_to_csv, files),
+                                     max_value=len(files)):
+        pass
+
+    pool.close()
+    pool.join()
