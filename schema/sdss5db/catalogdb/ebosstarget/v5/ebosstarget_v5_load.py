85b2a14068d0388393b5933f5d8648c05f3bb135
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-16 04:38:29 -0700

    Fix incorrect table

--- schema/sdss5db/catalogdb/ebosstarget/v5/ebosstarget_v5_load.py
+++ schema/sdss5db/catalogdb/ebosstarget/v5/ebosstarget_v5_load.py
@@ -35,7 +35,7 @@ def main():
         cursor = database.cursor()
         fileobj = open(out + file_ + '.csv')
         fileobj.readline()  # Read header
-        cursor.copy_from(fileobj, 'catalogdb.gaia_unwise_agn', sep=',')
+        cursor.copy_from(fileobj, 'catalogdb.ebosstarget_v5', sep=',')
         database.commit()
 
 
