1cc4d66371d21e1f520a898c6d2254416d9386e5
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-20 11:23:34 -0600

    Fix tycho2 q3c cluster

--- schema/sdss5db/catalogdb/tycho2/tycho2_index.sql
+++ schema/sdss5db/catalogdb/tycho2/tycho2_index.sql
@@ -1,4 +1,4 @@
 
 CREATE INDEX on catalogdb.tycho2 (q3c_ang2ipix(ramdeg, demdeg));
-CLUSTER legacy_survey_q3c_ang2ipix_idx on catalogdb.tycho2;
+CLUSTER tycho2_q3c_ang2ipix_idx on catalogdb.tycho2;
 ANALYZE catalogdb.tycho2;
