f2301099c7dbddc84e5cbc96c6bb05a2c9b86dd1
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-20 11:24:12 -0600

    Add indexes to tmass_best_neighbour fks

--- schema/sdss5db/catalogdb/foreignKeys.sql
+++ schema/sdss5db/catalogdb/foreignKeys.sql
@@ -9,12 +9,6 @@ https://www.postgresql.org/docs/8.2/static/sql-altertable.html
 
 */
 
-ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
-    ADD CONSTRAINT original_ext_source_id_fk
-    FOREIGN KEY (original_ext_source_id)
-    REFERENCES catalogdb.twomass_psc (designation)
-    ON UPDATE CASCADE ON DELETE CASCADE;
-
 ALTER TABLE catalogdb.gaia_dr2_wd_candidates_v1
     ADD CONSTRAINT source_id_fk
     FOREIGN KEY (source_id)
@@ -27,11 +21,15 @@ ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
     REFERENCES catalogdb.gaia_dr2_source (source_id)
     ON UPDATE CASCADE ON DELETE CASCADE;
 
+CREATE INDEX ON catalogdb.gaiadr2_tmass_best_neighbour using BTREE (source_id ASC);
+
 ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
     ADD CONSTRAINT tmass_pts_key_fk
     FOREIGN KEY (tmass_pts_key)
     REFERENCES catalogdb.twomass_psc (pts_key)
     ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE INDEX ON catalogdb.gaiadr2_tmass_best_neighbour using BTREE (tmass_pts_key ASC);
 
 ALTER TABLE catalogdb.sdss_dr14_specobj
     ADD CONSTRAINT bestobjid_fk
