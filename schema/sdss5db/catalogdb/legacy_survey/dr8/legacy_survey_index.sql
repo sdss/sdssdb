46783bf52e205ad2749de9898de640470115f2a4
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-28 15:00:05 -0600

    Make ls_id the pk

--- schema/sdss5db/catalogdb/legacy_survey/dr8/legacy_survey_index.sql
+++ schema/sdss5db/catalogdb/legacy_survey/dr8/legacy_survey_index.sql
@@ -1,10 +1,5 @@
 
-CREATE UNIQUE INDEX CONCURRENTLY ls_id_pk
-    ON catalogdb.legacy_survey_dr8 (ls_id);
-
-ALTER TABLE catalogdb.legacy_survey_dr8
-    ADD CONSTRAINT ls_id_pk PRIMARY KEY
-    USING INDEX ls_id_pk;
+ALTER TABLE catalogdb.legacy_survey_dr8 ADD PRIMARY KEY (ls_id);
 
 CREATE INDEX ON catalogdb.legacy_survey_dr8 (q3c_ang2ipix(ra, dec));
 CLUSTER legacy_survey_q3c_ang2ipix_idx ON catalogdb.legacy_survey_dr8;
