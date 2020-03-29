32595f86534c07e225c373865f1a4e381484ce6c
José Sánchez-Gallego <gallegoj@uw.edu>
2020-03-28 19:42:50 -0600

    Remove some indexes from gaia

--- schema/sdss5db/catalogdb/gaia/dr2/gaia_dr2_source_index.sql
+++ schema/sdss5db/catalogdb/gaia/dr2/gaia_dr2_source_index.sql
@@ -11,12 +11,6 @@ drop index catalogdb.gaia_dr1_tgas_dec_index;
 
 -- Indices
 
-CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (ra);
-CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (dec);
-CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (l);
-CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (b);
-CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (ecl_lon);
-CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (ecl_lat);
 CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (phot_g_mean_flux);
 CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (phot_g_mean_mag);
 CREATE INDEX ON catalogdb.gaia_dr2_source USING BTREE (solution_id);
