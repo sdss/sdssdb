2cee5707c488f5d1d88e3cfd43b30d8f9c1cb6bf
José Sánchez-Gallego <gallegoj@uw.edu>
2020-04-02 18:08:20 -0600

    Add tycho2 unique index on name

--- schema/sdss5db/catalogdb/tycho2/tycho2_index.sql
+++ schema/sdss5db/catalogdb/tycho2/tycho2_index.sql
@@ -1,4 +1,9 @@
 
+CREATE UNIQUE INDEX CONCURRENTLY catalogdb_tycho2_name_unique_idx ON catalogdb.tycho2 (name);
+ALTER TABLE catalogdb.tycho2
+    ADD CONSTRAINT catalogdb_tycho2_name_unique
+    UNIQUE USING INDEX catalogdb_tycho2_name_unique_idx;
+
 CREATE INDEX on catalogdb.tycho2 (q3c_ang2ipix(ramdeg, demdeg));
 CLUSTER tycho2_q3c_ang2ipix_idx on catalogdb.tycho2;
 ANALYZE catalogdb.tycho2;
