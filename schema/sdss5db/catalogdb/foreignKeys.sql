814ab785b9ac5d249a127d420201006fadd2c6fe
José Sánchez-Gallego <gallegoj@uw.edu>
2020-06-16 13:42:35 -0700

    Change catalogdb FKs to initialy deferred

--- schema/sdss5db/catalogdb/foreignKeys.sql
+++ schema/sdss5db/catalogdb/foreignKeys.sql
@@ -16,7 +16,7 @@ ALTER TABLE catalogdb.gaia_dr2_clean
     ADD CONSTRAINT source_id_fk
     FOREIGN KEY (source_id)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- gaiadr2_sdssdr9_best_neighbour
@@ -30,13 +30,13 @@ ALTER TABLE catalogdb.gaia_dr2_clean
 --     ADD CONSTRAINT source_id_fk
 --     FOREIGN KEY (source_id)
 --     REFERENCES catalogdb.gaia_dr2_source (source_id)
---     ON UPDATE CASCADE ON DELETE CASCADE;
+--     DEFERRABLE INITIALLY DEFERRED;
 
 -- ALTER TABLE catalogdb.gaiadr2_sdssdr9_best_neighbour
 --     ADD CONSTRAINT sdssdr9_oid_fk
 --     FOREIGN KEY (sdssdr9_oid)
 --     REFERENCES catalogdb.sdss_dr13_photoobj (objid)
---     ON UPDATE CASCADE ON DELETE CASCADE;
+--     DEFERRABLE INITIALLY DEFERRED;
 
 
 -- gaiadr2_tmass_best_neighbour
@@ -47,13 +47,13 @@ ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
     ADD CONSTRAINT source_id_fk
     FOREIGN KEY (source_id)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
     ADD CONSTRAINT tmass_pts_key_fk
     FOREIGN KEY (tmass_pts_key)
     REFERENCES catalogdb.twomass_psc (pts_key)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- gaiadr2_tmass_best_neighbour
@@ -64,13 +64,13 @@ ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
     ADD CONSTRAINT source_id_fk
     FOREIGN KEY (source_id)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.gaiadr2_tmass_best_neighbour
     ADD CONSTRAINT tmass_pts_key_fk
     FOREIGN KEY (tmass_pts_key)
     REFERENCES catalogdb.twomass_psc (pts_key)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- sdss_dr14_specobj
@@ -83,7 +83,7 @@ CREATE INDEX CONCURRENTLY ON catalogdb.sdss_dr14_specobj using BTREE (bestobjid 
 --     ADD CONSTRAINT bestobjid_fk
 --     FOREIGN KEY (bestobjid)
 --     REFERENCES catalogdb.sdss_dr13_photoobj (objid)
---     ON UPDATE CASCADE ON DELETE CASCADE;
+--     DEFERRABLE INITIALLY DEFERRED;
 
 -- sdss_dr16_specobj
 
@@ -95,7 +95,7 @@ CREATE INDEX CONCURRENTLY ON catalogdb.sdss_dr16_specobj using BTREE (bestobjid 
 --     ADD CONSTRAINT bestobjid_fk
 --     FOREIGN KEY (bestobjid)
 --     REFERENCES catalogdb.sdss_dr13_photoobj (objid)
---     ON UPDATE CASCADE ON DELETE CASCADE;
+--     DEFERRABLE INITIALLY DEFERRED;
 
 
 -- gaia_unwise_agn
@@ -106,13 +106,13 @@ ALTER TABLE catalogdb.gaia_unwise_agn
     ADD CONSTRAINT source_id_fk
     FOREIGN KEY (gaia_sourceid)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.gaia_unwise_agn
     ADD CONSTRAINT unwise_objid_fk
     FOREIGN KEY (unwise_objid)
     REFERENCES catalogdb.unwise (unwise_objid)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- sdss_dr14_apogeeStarVisit
@@ -121,13 +121,13 @@ ALTER TABLE catalogdb.sdss_dr14_apogeeStarVisit
     ADD CONSTRAINT visit_id_fk
     FOREIGN KEY (visit_id)
     REFERENCES catalogdb.sdss_dr14_apogeeVisit (visit_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.sdss_dr14_apogeeStarVisit
     ADD CONSTRAINT apstar_id_fk
     FOREIGN KEY (apstar_id)
     REFERENCES catalogdb.sdss_dr14_apogeeStar (apstar_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- sdss_dr14_ascapStar
@@ -136,7 +136,7 @@ ALTER TABLE catalogdb.sdss_dr14_ascapStar
     ADD CONSTRAINT apstar_id_fk
     FOREIGN KEY (apstar_id)
     REFERENCES catalogdb.sdss_dr14_apogeeStar (apstar_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- tic_v8
@@ -152,37 +152,37 @@ ALTER TABLE catalogdb.tic_v8
     ADD CONSTRAINT sdss_fk
     FOREIGN KEY (sdss)
     REFERENCES catalogdb.sdss_dr13_photoobj (objid)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.tic_v8
     ADD CONSTRAINT tyc_fk
     FOREIGN KEY (tyc)
     REFERENCES catalogdb.tycho2 (designation)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.tic_v8
     ADD CONSTRAINT twomass_psc_fk
     FOREIGN KEY (twomass_psc)
     REFERENCES catalogdb.twomass_psc (designation)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.tic_v8
     ADD CONSTRAINT allwise_fk
     FOREIGN KEY (allwise)
     REFERENCES catalogdb.allwise (designation)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.tic_v8
     ADD CONSTRAINT kic_fk
     FOREIGN KEY (kic)
     REFERENCES catalogdb.kepler_input_10 (kic_kepler_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.tic_v8
     ADD CONSTRAINT gaia_int_fk
     FOREIGN KEY (gaia_int)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- bhm_spiders_agn_superset, bhm_spiders_clusters_superset
@@ -195,13 +195,13 @@ ALTER TABLE catalogdb.bhm_spiders_agn_superset
     ADD CONSTRAINT gaia_dr2_source_id_fk
     FOREIGN KEY (gaia_dr2_source_id)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.bhm_spiders_agn_superset
     ADD CONSTRAINT ls_id_fk
     FOREIGN KEY (ls_id)
     REFERENCES catalogdb.legacy_survey_dr8 (ls_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 CREATE INDEX CONCURRENTLY ON catalogdb.bhm_spiders_clusters_superset USING BTREE (gaia_dr2_source_id);
 CREATE INDEX CONCURRENTLY ON catalogdb.bhm_spiders_clusters_superset USING BTREE (ls_id);
@@ -211,13 +211,13 @@ ALTER TABLE catalogdb.bhm_spiders_clusters_superset
     ADD CONSTRAINT gaia_dr2_source_id_fk
     FOREIGN KEY (gaia_dr2_source_id)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.bhm_spiders_clusters_superset
     ADD CONSTRAINT ls_id_fk
     FOREIGN KEY (ls_id)
     REFERENCES catalogdb.legacy_survey_dr8 (ls_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- skymapper_dr1_1
@@ -230,19 +230,19 @@ ALTER TABLE catalogdb.skymapper_dr1_1
     ADD CONSTRAINT allwise_cntr_fk
     FOREIGN KEY (allwise_cntr)
     REFERENCES catalogdb.allwise (cntr)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.skymapper_dr1_1
     ADD CONSTRAINT gaia_dr2_id1_fk
     FOREIGN KEY (gaia_dr2_id1)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE catalogdb.skymapper_dr1_1
     ADD CONSTRAINT gaia_dr2_id2_fk
     FOREIGN KEY (gaia_dr2_id2)
     REFERENCES catalogdb.gaia_dr2_source (source_id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- glimpse
@@ -253,7 +253,7 @@ ALTER TABLE catalogdb.glimpse
     ADD CONSTRAINT tmass_cntr_fk
     FOREIGN KEY (tmass_cntr)
     REFERENCES catalogdb.twomass_psc (pts_key)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    DEFERRABLE INITIALLY DEFERRED;
 
 
 -- gaia_dr2_wd
