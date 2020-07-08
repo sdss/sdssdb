6c3aa13da6211fd5f6a1ef0c1db20011e836d917
José Sánchez-Gallego <gallegoj@uw.edu>
2020-07-07 19:26:04 -0600

    Add some columns to tic_v8_lite

--- schema/sdss5db/catalogdb/tic/v8/tic_v8_lite.sql
+++ schema/sdss5db/catalogdb/tic/v8/tic_v8_lite.sql
@@ -1,39 +1,76 @@
 
 CREATE MATERIALIZED VIEW catalogdb.tic_v8_lite AS
-    SELECT
-        id,
-	    hip,
-	    tyc,
-        twomass_psc,
-	    sdss,
-	    allwise,
-	    kic,
-        gaia_int,
-        ra,
-        dec,
-        posflag,
-        pmra,
-        pmdec,
-        pmflag,
-        plx,
-        e_plx,
-        parflag,
-        gallong,
-        gallat,
-        eclong,
-        eclat,
-        ebv,
-        e_ebv,
-        gaiamag,
-        gaiabp,
-        gaiarp,
-        ra_orig,
-        dec_orig
-    FROM catalogdb.tic_v8
-    ORDER BY q3c_ang2ipix(ra, dec);
+    id,
+    version,
+    hip,
+    tyc,
+    ucac,
+    twomass,
+    sdss,
+    allwise,
+    gaia,
+    apass,
+    kic,
+    objtype,
+    typesrc,
+    ra,
+    dec,
+    posflag,
+    pmra,
+    pmdec,
+    pmflag,
+    plx,
+    e_plx,
+    parflag,
+    gallong,
+    gallat,
+    eclong,
+    eclat,
+    bmag,
+    vmag,
+    umag,
+    gmag,
+    rmag,
+    imag,
+    zmag,
+    jmag,
+    hmag,
+    kmag,
+    twomflag,
+    prox,
+    w1mag,
+    w2mag,
+    w3mag,
+    w4mag,
+    gaiamag,
+    tmag,
+    tessflag,
+    spflag,
+    teff,
+    logg,
+    mh,
+    rad,
+    mass,
+    rho,
+    lumclass,
+    lum,
+    d,
+    ebv,
+    gaiabp,
+    gaiarp,
+    ra_orig,
+    dec_orig
+FROM catalogdb.tic_v8;
 
 
 CREATE UNIQUE INDEX on catalogdb.tic_v8_lite (id);
+
+CREATE INDEX ON catalogdb.tic_v8_lite (jmag);
+CREATE INDEX ON catalogdb.tic_v8_lite (hmag);
+CREATE INDEX ON catalogdb.tic_v8_lite (kmag);
+CREATE INDEX ON catalogdb.tic_v8_lite (tmag);
+
+CREATE INDEX ON catalogdb.tic_v8_lite (plx);
 
 CREATE INDEX ON catalogdb.tic_v8_lite (gaia_int);
 CREATE INDEX ON catalogdb.tic_v8_lite (tyc);
