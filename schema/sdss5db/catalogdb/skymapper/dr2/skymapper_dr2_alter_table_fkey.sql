e5a4e449cbcafc967ce6f088007b7b5049b75a65
Pramod Gupta <psgupta@astro.washington.edu>
2020-10-09 18:00:51 -0600

    intial commit of skymapper_dr2_alter_table_fkey.sql

---
+++ schema/sdss5db/catalogdb/skymapper/dr2/skymapper_dr2_alter_table_fkey.sql
@@ -0,0 +1,12 @@
+ALTER TABLE ONLY catalogdb.skymapper_dr2
+    ADD FOREIGN KEY (allwise_cntr)
+    REFERENCES catalogdb.allwise(cntr);
+    
+ALTER TABLE ONLY catalogdb.skymapper_dr2
+    ADD FOREIGN KEY (gaia_dr2_id2)
+    REFERENCES catalogdb.gaia_dr2_source(source_id);
+
+ALTER TABLE ONLY catalogdb.skymapper_dr2
+    ADD FOREIGN KEY (gaia_dr2_id1)
+    REFERENCES catalogdb.gaia_dr2_source(source_id);    
+
