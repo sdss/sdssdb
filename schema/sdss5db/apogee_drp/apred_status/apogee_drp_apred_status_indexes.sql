48fd3d95b23b7fcd202ad7da958fecc75aabf4b6
David Nidever <dnidever@montana.edu>
2020-10-15 23:47:55 -0600

    Renamed some files.

--- schema/sdss5db/apogee_drp/apred_status/apogee_drp_apred_status_indexes.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX CONCURRENTLY ON apogee_drp.apred_status USING BTREE (instrument);
-CREATE INDEX CONCURRENTLY ON apogee_drp.apred_status USING BTREE (telescope);
-CREATE INDEX CONCURRENTLY ON apogee_drp.apred_status USING BTREE (mjd);
-CREATE INDEX CONCURRENTLY ON apogee_drp.apred_status USING BTREE (plate);
-
-
