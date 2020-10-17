8622da5f2adda3dd79fceece3de3440bcd48ce27
David Nidever <dnidever@montana.edu>
2020-10-17 01:18:23 -0600

    Adding schema files for apogee_drp.rv_status.

---
+++ schema/sdss5db/apogee_drp/rv_status/apogee_drp_rv_status.sql
@@ -0,0 +1,19 @@
+/*
+
+APOGEE DRP RV_STATUS information
+
+*/
+
+CREATE TABLE apogee_drp.rv_status (
+    PK SERIAL NOT NULL PRIMARY KEY,
+    APOGEE_ID    text,
+    APRED_VERS  text,
+    TELESCOPE   text,
+    HEALPIX	integer,
+    NVISITS	integer,
+    PBSKEY      text,
+    FILE	TEXT,
+    CHECKTIME   timestamp,
+    SUCCESS	boolean,
+    CREATED timestamp with time zone DEFAULT now() NOT NULL
+);
---
+++ schema/sdss5db/apogee_drp/rv_status/apogee_drp_rv_status_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX CONCURRENTLY ON apogee_drp.rv_status USING BTREE (apogee_id);
+CREATE INDEX CONCURRENTLY ON apogee_drp.rv_status USING BTREE (apred_vers);
+CREATE INDEX CONCURRENTLY ON apogee_drp.rv_status USING BTREE (telescope);
