525414ca156aac63ad5a67162a45b9d417824c9d
David Nidever <dnidever@montana.edu>
2020-10-27 03:40:22 -0600

    Fixed apogee_dr.visit_latest to use a left join so it shows visit information for star that failed on RV.

--- schema/sdss5db/apogee_drp/visit_latest/apogee_drp_visit_latest.sql
+++ schema/sdss5db/apogee_drp/visit_latest/apogee_drp_visit_latest.sql
@@ -5,6 +5,12 @@ APOGEE DRP visit_latest VIEW
 */
 
 CREATE VIEW apogee_drp.visit_latest AS
+  WITH rv as (
+    SELECT * from apogee_drp.rv_visit as rr
+    WHERE (rr.apogee_id, rr.apred_vers, rr.telescope, rr.starver) IN
+      (SELECT apogee_id, apred_vers, telescope, max(starver) FROM apogee_drp.rv_visit 
+       GROUP BY apogee_id, apred_vers, telescope)
+  )
   SELECT v.apogee_id,v.target_id,v.catalogid,v.apred_vers,v.file,v.uri,v.fiberid,v.plate,v.mjd,v.telescope,
          v.survey,v.field,v.programname,v.ra,v.dec,v.glon,v.glat,v.jmag,
          v.jerr,v.hmag,v.herr,v.kmag,v.kerr,v.src_h,v.pmra,v.pmdec,v.pm_src,
@@ -15,7 +21,4 @@ CREATE VIEW apogee_drp.visit_latest AS
          rv.starver,rv.bc,rv.vtype,rv.vrel,rv.vrelerr,rv.vheliobary,rv.chisq,rv.rv_teff,
          rv.rv_feh,rv.rv_logg,rv.xcorr_vrel,rv.xcorr_vrelerr,rv.xcorr_vheliobary,rv.n_components,
          rv.rv_components
-  FROM apogee_drp.rv_visit AS rv JOIN apogee_drp.visit AS v ON rv.visit_pk=v.pk
-  WHERE (rv.apogee_id, rv.apred_vers, rv.telescope, rv.starver) IN
-    (SELECT apogee_id, apred_vers, telescope, max(starver) FROM apogee_drp.rv_visit 
-     GROUP BY apogee_id, apred_vers, telescope);
+  FROM apogee_drp.visit AS v LEFT JOIN rv ON rv.visit_pk=v.pk;
