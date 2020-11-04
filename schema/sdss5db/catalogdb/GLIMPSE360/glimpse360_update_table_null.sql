86a97d5ca88e5a8cbb04ce70c3abeaf8396b6b53
Pramod Gupta <psgupta@astro.washington.edu>
2020-11-04 16:51:41 -0700

    initial commit of glimpse360_update_table_null.sql

---
+++ schema/sdss5db/catalogdb/GLIMPSE360/glimpse360_update_table_null.sql
@@ -0,0 +1,7 @@
+-- update table catalogdb.glimpse360 and set field to NULL based on
+-- page 26 table 8 of the below document
+-- http://www.astro.wisc.edu/sirtf/glimpse360_dataprod_v1.2.pdf
+
+update catalogdb.glimpse360 set tmass_cntr=NULL where tmass_cntr=0 ;
+
+update catalogdb.glimpse360 set tmass_designation=NULL where tmass_designation like'%null%' ;
