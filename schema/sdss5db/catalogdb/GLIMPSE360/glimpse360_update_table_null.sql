5b9914e19e15d6bf3ded4ecbdd9200509c54b801
Pramod Gupta <psgupta@astro.washington.edu>
2020-11-06 17:47:50 -0700

    edit glimpse360_update_table_null.sql

--- schema/sdss5db/catalogdb/GLIMPSE360/glimpse360_update_table_null.sql
+++ schema/sdss5db/catalogdb/GLIMPSE360/glimpse360_update_table_null.sql
@@ -2,6 +2,10 @@
 -- page 26 table 8 of the below document
 -- http://www.astro.wisc.edu/sirtf/glimpse360_dataprod_v1.2.pdf
 
+\o glimpse360_update_table_null.out
+
 update catalogdb.glimpse360 set tmass_cntr=NULL where tmass_cntr=0 ;
 
 update catalogdb.glimpse360 set tmass_designation=NULL where tmass_designation like'%null%' ;
+
+\o
