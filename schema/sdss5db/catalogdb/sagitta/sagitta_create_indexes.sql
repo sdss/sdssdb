1e4af2f54c9df124ab827cd5f15ca76dda8d60f0
Pramod Gupta <psgupta@astro.washington.edu>
2020-11-08 21:13:08 -0700

    edit sagitta_create_indexes.sql

--- schema/sdss5db/catalogdb/sagitta/sagitta_create_indexes.sql
+++ schema/sdss5db/catalogdb/sagitta/sagitta_create_indexes.sql
@@ -1,6 +1,6 @@
 \o sagitta_create_indexes.out
 create index on catalogdb.sagitta(q3c_ang2ipix(ra,dec));
-create index on catalogdb.sagitta(av real);
-create index on catalogdb.sagitta(yso real);
+create index on catalogdb.sagitta(av);
+create index on catalogdb.sagitta(yso);
 create index on catalogdb.sagitta(yso_std);
 \o
