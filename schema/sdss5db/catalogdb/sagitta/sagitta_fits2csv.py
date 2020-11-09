23a1d1acd1db92b248dfb0c9860da0f7846254ad
Pramod Gupta <psgupta@astro.washington.edu>
2020-11-08 21:21:01 -0700

    edit sagitta_fits2csv.py

--- schema/sdss5db/catalogdb/sagitta/sagitta_fits2csv.py
+++ schema/sdss5db/catalogdb/sagitta/sagitta_fits2csv.py
@@ -1,9 +1,12 @@
 from astropy.io import fits
+
+
 hdu = fits.open('sagitta-sdssv.fits', memmap=True)
 data = hdu[1].data
 num_lines_fits = len(data)
 
 ftemp = open('temp1', 'w')
+
 for i in range(num_lines_fits):
     print(data[i], file=ftemp)
 
