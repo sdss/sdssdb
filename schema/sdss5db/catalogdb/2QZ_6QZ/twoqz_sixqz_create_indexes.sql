22084afbb85c543fc210f6999b5fbac59b20f474
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2020-11-10 10:43:20 -0800

    2qz 6qz (#48)
    * initial commit of sagitta_create_table.sql and sagitta_fits2csv.py
    
    * initial commit of sagitta_load.sql, sagitta_alter_table_pkey.sql, and sagitta_create_indexes.sql
    
    * updates
    
    * initial commit of twoqz_sixqz *.py and *.sql files
    
    * edit twoqz_sixqz_alter_table_pkey.sql and twoqz_sixqz_create_indexes.sql

---
+++ schema/sdss5db/catalogdb/2QZ_6QZ/twoqz_sixqz_create_indexes.sql
@@ -0,0 +1,13 @@
+\o twoqz_sixqz_create_indexes.out
+create index on catalogdb.twoqz_sixqz(q3c_ang2ipix(ra_degree,dec_degree));
+create index on catalogdb.twoqz_sixqz(name);
+create index on catalogdb.twoqz_sixqz(bj);
+create index on catalogdb.twoqz_sixqz(u_bj);
+create index on catalogdb.twoqz_sixqz(bj_r);
+create index on catalogdb.twoqz_sixqz(z1);
+create index on catalogdb.twoqz_sixqz(q1);
+create index on catalogdb.twoqz_sixqz(id1);
+create index on catalogdb.twoqz_sixqz(z2);
+create index on catalogdb.twoqz_sixqz(q2);
+create index on catalogdb.twoqz_sixqz(id2);
+\o
