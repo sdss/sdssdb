699215bffa38ecd4060840721cab9ea16bf18d60
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2020-11-16 11:25:14 -0800

    Zari18 (#49)
    * initial commit of sagitta_create_table.sql and sagitta_fits2csv.py
    
    * initial commit of sagitta_load.sql, sagitta_alter_table_pkey.sql, and sagitta_create_indexes.sql
    
    * updates
    
    * initial commit of files for catalogdb.zari18pms and catalogdb.zari18ums

---
+++ schema/sdss5db/catalogdb/zari18/zari18ums_create_indexes.sql
@@ -0,0 +1,22 @@
+\o zari18ums_create_indexes.out
+create index on catalogdb.zari18ums(source);
+create index on catalogdb.zari18ums(glon);
+create index on catalogdb.zari18ums(glat);
+create index on catalogdb.zari18ums(plx);
+create index on catalogdb.zari18ums(e_plx);
+create index on catalogdb.zari18ums(pmglon);
+create index on catalogdb.zari18ums(e_pmglon);
+create index on catalogdb.zari18ums(pmglat);
+create index on catalogdb.zari18ums(e_pmglat);
+create index on catalogdb.zari18ums(pmlbcorr);
+create index on catalogdb.zari18ums(rv);
+create index on catalogdb.zari18ums(e_rv);
+create index on catalogdb.zari18ums(gmag);
+create index on catalogdb.zari18ums(bpmag);
+create index on catalogdb.zari18ums(rpmag);
+create index on catalogdb.zari18ums(bp_over_rp);
+create index on catalogdb.zari18ums(chi2al);
+create index on catalogdb.zari18ums(ngal);
+create index on catalogdb.zari18ums(ag);
+create index on catalogdb.zari18ums(bp_rp);
+\o
