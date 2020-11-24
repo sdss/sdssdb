84c9df5fd9ef183285d18ea686f356dd49d90e48
Pramod Gupta <psgupta@astro.washington.edu>
2020-11-23 19:33:38 -0700

    SQL and Python files for table catalogdb.cantat_gaudin_nodup

--- schema/sdss5db/catalogdb/cantat_gaudin/cantat_gaudin_nodup_create_indexes.sql
+++ schema/sdss5db/catalogdb/cantat_gaudin/cantat_gaudin_nodup_create_indexes.sql
@@ -1,13 +1,6 @@
 \o cantat_gaudin_nodup_create_indexes.out
 create index on catalogdb.cantat_gaudin_nodup(q3c_ang2ipix(radeg,dedeg));
 create index on catalogdb.cantat_gaudin_nodup(gaiadr2);
-create index on catalogdb.cantat_gaudin_nodup(glon);
-create index on catalogdb.cantat_gaudin_nodup(glat);
-create index on catalogdb.cantat_gaudin_nodup(plx);
-create index on catalogdb.cantat_gaudin_nodup(rv);
-create index on catalogdb.cantat_gaudin_nodup(o_gmag);
-create index on catalogdb.cantat_gaudin_nodup(gmag);
-create index on catalogdb.cantat_gaudin_nodup(bp_rp);
 create index on catalogdb.cantat_gaudin_nodup(proba);
 create index on catalogdb.cantat_gaudin_nodup(cluster);
 \o
--- schema/sdss5db/catalogdb/cantat_gaudin/cantat_gaudin_nodup_load.sql
+++ schema/sdss5db/catalogdb/cantat_gaudin/cantat_gaudin_nodup_load.sql
@@ -1,7 +1,5 @@
--- sed "s/'//g" nodup.csv > nodup_noquote.csv
--- sed 's/ //g' nodup_noquote.csv > nodup_noquote2.csv
 \o cantat_gaudin_nodup_load.out
-\copy catalogdb.cantat_gaudin_nodup from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/cantat_gaudin/nodup_noquote2.csv' delimiter ',' ;
+\copy catalogdb.cantat_gaudin_nodup from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/cantat_gaudin/nodup.csv' delimiter ',' ;
 \o
 
 
