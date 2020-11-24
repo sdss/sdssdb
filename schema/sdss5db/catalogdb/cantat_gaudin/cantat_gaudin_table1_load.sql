2e1b159bf326d91999f2068078c39c541a9b0c41
Pramod Gupta <psgupta@astro.washington.edu>
2020-11-24 08:23:46 -0700

    add cantat_gaudin README

--- schema/sdss5db/catalogdb/cantat_gaudin/cantat_gaudin_table1_load.sql
+++ schema/sdss5db/catalogdb/cantat_gaudin/cantat_gaudin_table1_load.sql
@@ -1,5 +1,3 @@
 \o cantat_gaudin_table1_load.out
 \copy catalogdb.cantat_gaudin_table1 from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/cantat_gaudin/table1.csv' delimiter ',' null '---' ;
 \o
-
-
