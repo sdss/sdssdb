857b87e17c5757128d61dbf17b5575a775aabcb5
Pramod Gupta <psgupta@astro.washington.edu>
2020-12-01 13:38:35 -0700

    bhm_rm_tweaks: initial commit of *sql and *py files

---
+++ schema/sdss5db/catalogdb/bhm_rm/v0.5/bhm_rm_tweaks_create_table.sql
@@ -0,0 +1,18 @@
+-- Based on the information in the below SDSS wiki page
+-- Updates+to+BHM+RM+catalogs+and+cartons+in+v0.5
+
+create table catalogdb.bhm_rm_tweaks (
+rm_field_name char(12) not null,
+plate integer,
+fiberid integer,
+mjd integer,
+catalogid bigint not null,
+ra double precision not null,
+dec double precision,
+rm_suitability integer,
+in_plate boolean,
+firstcarton char(17),
+mag real[5],
+gaia_g real,
+date_set char(11) 
+);
