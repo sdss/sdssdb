2788135073f0289419326d5ec225f1f14ce2c207
Pramod Gupta <psgupta@astro.washington.edu>
2020-12-03 19:43:46 -0700

    tess_toi v0.5: initial commit of *sql and *py

---
+++ schema/sdss5db/catalogdb/TESS-TOI/v0.5/tess_toi_alter_table_pkey.sql
@@ -0,0 +1,3 @@
+\o tess_toi_alter_table_pkey.out
+alter table catalogdb.tess_toi add column pkey bigserial primary key;
+\o
---
+++ schema/sdss5db/catalogdb/TESS-TOI/v0.5/tess_toi_load.sql
@@ -0,0 +1,3 @@
+\o tess_toi_load.out
+\copy catalogdb.tess_toi from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/tess_toi/v0.5/TESS_Observedv0.5.csv' delimiter ',';
+\o
