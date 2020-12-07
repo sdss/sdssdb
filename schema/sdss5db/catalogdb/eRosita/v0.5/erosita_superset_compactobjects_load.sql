82eebf1230e9da8ec21291d4ae1beb45a57584f4
Pramod Gupta <psgupta@astro.washington.edu>
2020-12-07 12:02:27 -0700

    updates for erosita v0.5

--- schema/sdss5db/catalogdb/eRosita/v0.5/erosita_superset_compactobjects_load.sql
+++ schema/sdss5db/catalogdb/eRosita/v0.5/erosita_superset_compactobjects_load.sql
@@ -1,3 +1,4 @@
 \o erosita_superset_compactobjects_load.out
-\copy catalogdb.erosita_superset_compactobjects from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/eRosita/erosita_for_catalogdb_v0.5/sdssv_erosita_homogenise_superset_v0.1.1/csv/erosita_superset_compactobjects/sdssv_spiders_erass1_update_v05_stars_hamstars_gaia_v0.1.1.csv' delimiter ',';
+\copy catalogdb.erosita_superset_compactobjects from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/eRosita/erosita_for_catalogdb_v0.5/sdssv_erosita_homogenise_superset_v0.1.2/csv/erosita_superset_compactobjects/sdssv_spiders_erass1_update_v05_compact_gaiadr2_select1uniq_v0.1.2.csv' delimiter ',';
+\copy catalogdb.erosita_superset_compactobjects from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/eRosita/erosita_for_catalogdb_v0.5/sdssv_erosita_homogenise_superset_v0.1.2/csv/erosita_superset_compactobjects/sdssv_spiders_erass1_update_v05_compact_gaiadr2_select2univar_v0.1.2.csv' delimiter ',';
 \o
