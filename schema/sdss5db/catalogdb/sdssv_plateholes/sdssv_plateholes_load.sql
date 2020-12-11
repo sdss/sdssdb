f886625dc1b58e051218ffa23fbc503f4b090782
Pramod Gupta <psgupta@astro.washington.edu>
2020-12-11 09:17:56 -0700

    sdssv_plateholes: initial commit of *.sql and *.py files

---
+++ schema/sdss5db/catalogdb/sdssv_plateholes/sdssv_plateholes_load.sql
@@ -0,0 +1,3 @@
+\o sdssv_plateholes_load.out
+\copy catalogdb.sdssv_plateholes from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/sdssv_plateholes/update_09Dec2020/plateholes_STRUCT1.csv' delimiter ';';
+\o
---
+++ schema/sdss5db/catalogdb/sdssv_plateholes/sdssv_plateholes_meta_alter_table_pkey.sql
@@ -0,0 +1,3 @@
+\o sdssv_plateholes_meta_alter_table_pkey.out
+alter table catalogdb.sdssv_plateholes_meta add primary key (yanny_uid);
+\o
