de8494c73e81fd9d684d7ab109052d9164497b8d
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2020-12-11 18:23:43 -0800

    Sdssv plateholes (#63)
    * sdssv_plateholes: initial commit of *.sql and *.py files
    
    * add peewee model to catalogdb.py for tables sdssv_boss_conflist, sdssv_boss_spall, sdssv_plateholes, and sdssv_plateholes_meta
    
    * sdssv_plateholes: initial commit of sdssv_plateholes_alter_table_fkey.sql
    
    * sdssv_plateholes: initial commit of sdssv_plateholes_alter_table_fkey.sql

---
+++ schema/sdss5db/catalogdb/sdssv_plateholes/sdssv_plateholes_alter_table_pkey.sql
@@ -0,0 +1,3 @@
+\o sdssv_plateholes_alter_table_pkey.out
+alter table catalogdb.sdssv_plateholes add column pkey bigserial primary key;
+\o
---
+++ schema/sdss5db/catalogdb/sdssv_plateholes/sdssv_plateholes_meta_create_indexes.sql
@@ -0,0 +1,8 @@
+\o sdssv_plateholes_meta_create_indexes.out
+create index on catalogdb.sdssv_plateholes_meta(q3c_ang2ipix(racen,deccen));
+create index on catalogdb.sdssv_plateholes_meta(plateid);
+create index on catalogdb.sdssv_plateholes_meta(designid);
+create index on catalogdb.sdssv_plateholes_meta(locationid);
+create index on catalogdb.sdssv_plateholes_meta(programname);
+create index on catalogdb.sdssv_plateholes_meta(defaultsurveymode);
+\o
