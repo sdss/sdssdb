0ee595265b55055260508d063c865eaef77d37a2
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2021-04-15 09:08:45 -0700

    sdssv_boss_conflist and sdssv_boss_spall: updates for 12Apr2021 (#79)

--- schema/sdss5db/catalogdb/sdssv_boss/sdssv_boss_spall_load.sql
+++ schema/sdss5db/catalogdb/sdssv_boss/sdssv_boss_spall_load.sql
@@ -1,4 +1,4 @@
 \o sdssv_boss_spall_load.out
 -- delimiter is semicolon and not comma
-\copy catalogdb.sdssv_boss_spall from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/sdssv_boss/spAll-master.csv' delimiter ';';
+\copy catalogdb.sdssv_boss_spall from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/sdssv_boss/update2021apr12/spAll-master.csv' delimiter ';';
 \o 
