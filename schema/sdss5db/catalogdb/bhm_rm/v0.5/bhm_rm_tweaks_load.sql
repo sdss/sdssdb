8ad9602ff09123ad385f88dd1e9a6b0ae0a77cdf
Pramod Gupta <psgupta@astro.washington.edu>
2021-05-26 12:04:29 -0600

    catalogdb.bhm_rm_tweaks: updates for 2021 May 25

--- schema/sdss5db/catalogdb/bhm_rm/v0.5/bhm_rm_tweaks_load.sql
+++ schema/sdss5db/catalogdb/bhm_rm/v0.5/bhm_rm_tweaks_load.sql
@@ -1,3 +1,3 @@
 \o bhm_rm_tweaks_load.out
-\copy catalogdb.bhm_rm_tweaks from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/RM/bhm_rm_tweaks/csv/vi_rm_targets_catv0b.csv' delimiter ';';
+\copy catalogdb.bhm_rm_tweaks from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/RM/bhm_rm_tweaks/csv/vi_rm_targets_catv0.5.csv' delimiter ';';
 \o
