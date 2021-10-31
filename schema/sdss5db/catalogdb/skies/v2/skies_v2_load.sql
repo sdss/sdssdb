a05e57fe6ae48c5a16c379ec6eacb35ed9318f68
Pramod Gupta <psgupta@astro.washington.edu>
2021-10-30 19:48:25 -0600

    skies_v2: update catalogdb.py

--- schema/sdss5db/catalogdb/skies/v2/skies_v2_load.sql
+++ schema/sdss5db/catalogdb/skies/v2/skies_v2_load.sql
@@ -1,3 +1,3 @@
 \o skies_v2_load.out
-\copy catalogdb.skies_v2 from '/uufs/chpc.utah.edu/common/home/sdss05/sdsswork/users/u6030579/skies/skies.h5.csv' csv delimiter ',' null '' header quote '"';
+\copy catalogdb.skies_v2 from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/skies/v2/skies.h5.csv' csv delimiter ',' null '' header quote '"';
 \o
