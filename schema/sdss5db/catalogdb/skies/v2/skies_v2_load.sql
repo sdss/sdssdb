49f57112a3bcb4f53cf6e13d676fa08a3f94a2cd
Pramod Gupta <psgupta@astro.washington.edu>
2021-10-30 19:27:48 -0600

    skies_v2: .sql

---
+++ schema/sdss5db/catalogdb/skies/v2/skies_v2_load.sql
@@ -0,0 +1,3 @@
+\o skies_v2_load.out
+\copy catalogdb.skies_v2 from '/uufs/chpc.utah.edu/common/home/sdss05/sdsswork/users/u6030579/skies/skies.h5.csv' csv delimiter ',' null '' header quote '"';
+\o
