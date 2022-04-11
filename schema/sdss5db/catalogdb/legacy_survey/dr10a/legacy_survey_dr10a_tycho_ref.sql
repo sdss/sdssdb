d8b3ad74789ae691fe7ead6d5871768f78b86214
Pramod Gupta <psgupta@astro.washington.edu>
2022-04-11 11:18:19 -0600

    legacy_survey_dr10a: sql files

---
+++ schema/sdss5db/catalogdb/legacy_survey/dr10a/legacy_survey_dr10a_tycho_ref.sql
@@ -0,0 +1,11 @@
+-- See the below link for ref_cat values
+-- https://www.legacysurvey.org/dr8/files/
+
+\o legacy_survey_dr10a_tycho_ref.out
+
+alter table catalogdb.legacy_survey_dr10a add column tycho_ref bigint;
+
+update catalogdb.legacy_survey_dr10a set tycho_ref = ref_id
+    where ref_cat = 'T2';
+
+create index on catalogdb.legacy_survey_dr10a (tycho_ref);
