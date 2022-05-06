9191f8d7240e1d41921f0917ba4aa8736bf3d187
Pramod Gupta <psgupta@astro.washington.edu>
2022-05-06 17:52:03 -0600

    legacy_survey_dr10a_gaia_sourceid.sql: use ref_cat='GE'

--- schema/sdss5db/catalogdb/legacy_survey/dr10a/legacy_survey_dr10a_gaia_sourceid.sql
+++ schema/sdss5db/catalogdb/legacy_survey/dr10a/legacy_survey_dr10a_gaia_sourceid.sql
@@ -5,10 +5,10 @@
 
 alter table catalogdb.legacy_survey_dr10a add column gaia_sourceid bigint;
 
--- G2 is is used in legacy survey dr8
+-- G2 is used in legacy survey dr8
 -- G2 is for Gaia DR2
 --
--- GE isused in legacy survey dr10a and dr10
+-- GE is used in legacy survey dr10a and dr10
 -- GE is for Gaia EDR3
 update catalogdb.legacy_survey_dr10a set gaia_sourceid = ref_id
     where ref_cat = 'GE';
