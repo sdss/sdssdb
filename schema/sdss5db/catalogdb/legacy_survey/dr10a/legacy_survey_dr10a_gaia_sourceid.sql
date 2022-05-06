8b1f1422820fd7543d3e6b167dc6e61b83846376
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2022-05-06 16:55:39 -0700

    lsdr10a: legacy_survey_dr10a_gaia_sourceid.sql: use ref_cat='GE' (#120)
    * legacy_survey_dr10a_gaia_sourceid.sql: use ref_cat='GE'
    
    * legacy_survey_dr10a_gaia_sourceid.sql: use ref_cat='GE'

--- schema/sdss5db/catalogdb/legacy_survey/dr10a/legacy_survey_dr10a_gaia_sourceid.sql
+++ schema/sdss5db/catalogdb/legacy_survey/dr10a/legacy_survey_dr10a_gaia_sourceid.sql
@@ -5,7 +5,12 @@
 
 alter table catalogdb.legacy_survey_dr10a add column gaia_sourceid bigint;
 
+-- G2 is used in legacy survey dr8
+-- G2 is for Gaia DR2
+--
+-- GE is used in legacy survey dr10a and dr10
+-- GE is for Gaia EDR3
 update catalogdb.legacy_survey_dr10a set gaia_sourceid = ref_id
-    where ref_cat = 'G2';
+    where ref_cat = 'GE';
 
 create index on catalogdb.legacy_survey_dr10a (gaia_sourceid);
