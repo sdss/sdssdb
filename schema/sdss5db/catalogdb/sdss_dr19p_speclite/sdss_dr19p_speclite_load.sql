000e05d538fa7a64d42b1cb705474a32672f343d
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2022-07-25 18:18:50 -0700

    catalogdb.sdss_dr19p_speclite: add *.sql files, update catalogdb.py (#128)
    * catalogdb.sdss_dr19p_speclite: add *.sql files
    
    * catalogdb.py: update for sdss_dr19p_speclite

---
+++ schema/sdss5db/catalogdb/sdss_dr19p_speclite/sdss_dr19p_speclite_load.sql
@@ -0,0 +1,3 @@
+\o sdss_dr19p_speclite_load.out
+
+\copy catalogdb.sdss_dr19p_speclite from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/sdss_dr19p_speclite/sdss_dr19p_speclite.csv' csv header;
