9d760aa00fb6d27989c1160dae908bdc7f949b60
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2022-08-02 11:19:31 -0700

    galex_gr7_gaia_dr3: add *.sql and *.py files (#131)

---
+++ schema/sdss5db/catalogdb/galex_gr7_gaia_dr3/galex_gr7_gaia_dr3_alter_table_pkey.sql
@@ -0,0 +1,3 @@
+\o galex_gr7_gaia_dr3_alter_table_pkey.out
+alter table catalogdb.galex_gr7_gaia_dr3 add column pk bigserial primary key;
+\o
---
+++ schema/sdss5db/catalogdb/galex_gr7_gaia_dr3/galex_gr7_gaia_dr3_load.sql
@@ -0,0 +1,3 @@
+\o galex_gr7_gaia_dr3_load.out
+\copy catalogdb.galex_gr7_gaia_dr3 from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/galex_gr7_gaia_dr3/GR7_eDR3_arc3_lite.csv' delimiter ',';
+\o
