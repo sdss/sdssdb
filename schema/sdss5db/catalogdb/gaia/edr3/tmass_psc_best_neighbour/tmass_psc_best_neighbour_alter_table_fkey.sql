92196dcd9f1e08ce7a1aebeb6f4ba91b042ad1a4
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2022-08-20 12:17:31 -0700

    gaia_dr3: best_neighbours2 (#147)

--- schema/sdss5db/catalogdb/gaia/edr3/tmass_psc_best_neighbour/tmass_psc_best_neighbour_alter_table_fkey.sql
+++
@@ -1,4 +0,0 @@
-\o tmass_psc_best_neighbour_alter_table_fkey.out
-alter table catalogdb.gaia_edr3_tmass_psc_best_neighbour add foreign key (source_id) references catalogdb.gaia_dr3_source(source_id);
-alter table catalogdb.gaia_edr3_tmass_psc_best_neighbour add foreign key (original_ext_source_id) references catalogdb.twomass_psc(designation);
-\o
