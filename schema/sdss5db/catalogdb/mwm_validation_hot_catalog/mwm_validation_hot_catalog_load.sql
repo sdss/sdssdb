967fdfcde04773fadb791a3e96f1020cdf439bb9
Pramod Gupta <psgupta@astro.washington.edu>
2022-08-31 16:46:32 -0600

    remove mwm_validation_hot_catalog since it is replaced by a manual carton

--- schema/sdss5db/catalogdb/mwm_validation_hot_catalog/mwm_validation_hot_catalog_load.sql
+++
@@ -1,3 +0,0 @@
-\o mwm_validation_hot_catalog.out
-\copy catalogdb.mwm_validation_hot_catalog from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/mwm_validation_hot_catalog/mwm_validation_hot_catalog.csv' delimiter ',';
-\o
