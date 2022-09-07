63cf340c3cdf431dccf71b0f29c0ff1b976ef10a
Pramod Gupta <psgupta@astro.washington.edu>
2022-09-07 17:48:41 -0600

    sdss_dr17_apogee: add indexes

--- schema/sdss5db/catalogdb/sdss_dr17_apogee/sdss_dr17_apogee_allstarmerge_create_indexes.sql
+++ schema/sdss5db/catalogdb/sdss_dr17_apogee/sdss_dr17_apogee_allstarmerge_create_indexes.sql
@@ -1,4 +1,5 @@
 \o sdss_dr17_apogee_allstarmerge_create_indexes.out
 create index on catalogdb.sdss_dr17_apogee_allstarmerge(q3c_ang2ipix(ra, dec));
 create index on catalogdb.sdss_dr17_apogee_allstarmerge(q3c_ang2ipix(glon, glat));
+create index on catalogdb.sdss_dr17_apogee_allstarmerge(gaia_source_id);
 \o
