7aa185830905cf600202587360eeb82ae39cd14c
Pramod Gupta <psgupta@astro.washington.edu>
2022-10-27 17:44:47 -0600

    erosita v1: add *fkey.sql

---
+++ schema/sdss5db/catalogdb/eRosita/v1.0/erosita_superset_agn_fkey.sql
@@ -0,0 +1,13 @@
+\o erosita_superset_agn_fkey.out
+
+update catalogdb.erosita_superset_v1_agn set gaia_dr3_source_id = null 
+    where gaia_dr3_source_id = 0;
+alter table catalogdb.erosita_superset_v1_agn 
+    add foreign key (gaia_dr3_source_id) 
+    references catalogdb.gaia_dr3_source(source_id);
+
+update catalogdb.erosita_superset_v1_agn set ls_id = null where ls_id = 0;
+alter table catalogdb.erosita_superset_v1_agn 
+    add foreign key (ls_id) references catalogdb.legacy_survey_dr10(ls_id);
+
+\o
