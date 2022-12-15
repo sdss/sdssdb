d9221dd4450d3529cd9d7bb5517b4308910b3b1e
Tom Dwelly <dwelly@mpe.mpg.de>
2022-12-15 18:45:05 +0000

    added prototype for bhm_rm_v1 loading

---
+++ schema/sdss5db/catalogdb/bhm_rm/v1.0/bhm_rm_v1_fits2csv.py
@@ -0,0 +1,58 @@
+from astropy.io import fits
+
+
+# input_dir and output_dir must end with /
+input_dir = '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/RM/v1/'  # noqa: E501
+
+output_dir = '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/RM/v1/csv/'  # noqa: E501
+
+input_fits_file_list =\
+    ['bhm_rm_v1_0_29Nov2022.fits']
+
+for input_fits_file in input_fits_file_list:
+    output_csv_file = input_fits_file.replace('fits', 'csv')
+
+    hdu = fits.open(input_dir + input_fits_file, memmap=True)
+    data = hdu[1].data
+    num_lines_fits = len(data)
+
+    ftemp = open('temp1', 'w')
+
+    for i in range(num_lines_fits):
+        print(data[i], file=ftemp)
+
+    ftemp.close()
+
+    fin = open('temp1', 'r')
+    fout = open(output_dir + output_csv_file, 'w')
+
+    # We use ; as the CSV delimiter since comma
+    # is used as the delimiter for the array elements
+    for line in fin:
+        line = line.strip()
+        line = line.lstrip('(')
+        line = line.rstrip(')')
+        line = line.replace("'", "")
+        line = line.replace('array([    ', '')
+        line = line.replace('array([', '')
+        line = line.replace('], dtype=float32)', '')
+        line = line.replace(', ', ',')
+
+        tags = line.split(',')
+
+        line_out = ''
+        for i in range(10):
+            line_out = line_out + tags[i] + ';'
+
+        line_out = line_out + '{'
+        for i in range(10, 15):
+            line_out = line_out + tags[i] + ','
+
+        line_out = line_out.rstrip(',')
+        line_out = line_out + '};'
+        line_out = line_out + tags[15] + ';' + tags[16]
+
+        print(line_out, file=fout)
+
+    fin.close()
+    fout.close()
