c3641bf286982b125e992d6962a1a29a6a972a15
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2022-12-15 17:16:02 -0800

    added prototype for bhm_rm_v1 loading (#184)
    Co-authored-by: Tom Dwelly <dwelly@mpe.mpg.de>

---
+++ schema/sdss5db/catalogdb/bhm_rm/v1.0/bhm_rm_v1_create_indexes.sql
@@ -0,0 +1,20 @@
+\o bhm_rm_v1_create_indexes.out
+create index on catalogdb.bhm_rm_v1(q3c_ang2ipix(ra,dec));
+create index on catalogdb.bhm_rm_v1(ls_id_dr8);
+create index on catalogdb.bhm_rm_v1(ls_id_dr10);
+create index on catalogdb.bhm_rm_v1(gaia_dr2_source_id);
+create index on catalogdb.bhm_rm_v1(gaia_dr3_source_id);
+create index on catalogdb.bhm_rm_v1(panstarrs1_catid_objid);
+create index on catalogdb.bhm_rm_v1(rm_field_name);
+create index on catalogdb.bhm_rm_v1(catalogidv05);
+create index on catalogdb.bhm_rm_v1(rm_known_spec);
+create index on catalogdb.bhm_rm_v1(rm_core);
+create index on catalogdb.bhm_rm_v1(rm_var);
+create index on catalogdb.bhm_rm_v1(rm_ancillary);
+create index on catalogdb.bhm_rm_v1(rm_rm_xrayqso);
+create index on catalogdb.bhm_rm_v1(rm_unsuitable);
+create index on catalogdb.bhm_rm_v1(mag_g);
+create index on catalogdb.bhm_rm_v1(mag_r);
+create index on catalogdb.bhm_rm_v1(mag_i);
+create index on catalogdb.bhm_rm_v1(gaia_g);
+\o
---
+++ schema/sdss5db/catalogdb/bhm_rm/v1.0/bhm_rm_v1_load.sql
@@ -0,0 +1,3 @@
+\o bhm_rm_v1_load.out
+\copy catalogdb.bhm_rm_v1 from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/RM/v1/csv/bhm_rm_v1_0_29Nov2022.csv' delimiter ';' null 'NULL';
+\o
