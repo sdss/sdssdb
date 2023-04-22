5e5f880d1a3c88847ae81f3039ab6d27ce883d06
Pramod Gupta <psgupta@astro.washington.edu>
2023-04-22 10:39:27 -0600

    add index for gaia_dr3_nss_two_body_orbit

--- schema/sdss5db/catalogdb/gaia/dr3/nss_two_body_orbit/nss_two_body_orbit_create_indexes.sql
+++ schema/sdss5db/catalogdb/gaia/dr3/nss_two_body_orbit/nss_two_body_orbit_create_indexes.sql
@@ -1,2 +1,3 @@
 create index on catalogdb.gaia_dr3_nss_two_body_orbit(q3c_ang2ipix(ra,dec));
 create index on catalogdb.gaia_dr3_nss_two_body_orbit(source_id);
+create index on catalogdb.gaia_dr3_nss_two_body_orbit(period);
