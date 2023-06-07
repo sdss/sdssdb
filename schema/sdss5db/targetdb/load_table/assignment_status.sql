1c3f337606d4a3f99dbed3cc99b9294af6bee990
johndonor3 <j.donor@tcu.edu>
2023-06-07 08:58:21 -0600

    keep mjd for assignment_status

--- schema/sdss5db/targetdb/load_table/assignment_status.sql
+++ schema/sdss5db/targetdb/load_table/assignment_status.sql
@@ -8,8 +8,6 @@ ALTER TABLE ONLY targetdb.assignment_status
     ADD CONSTRAINT assignment_fk
     FOREIGN KEY (assignment_pk) REFERENCES targetdb.assignment(pk);
 
--- script that adds all current design.field_pk (and exposure/field_exposure) to new table
-
 insert into targetdb.assignment_status (pk, assignment_pk, status, mjd)
 select nextval('targetdb.assignment_status_pk_seq'), 
 asn.pk, d2s.completion_status_pk, d2s.mjd
@@ -21,5 +19,5 @@ where true ON CONFLICT DO NOTHING;
 update targetdb.assignment_status set status = 0, mjd = null
 where status = 1 or status = 2;
 
-update targetdb.assignment_status set status = 1, mjd = null
+update targetdb.assignment_status set status = 1
 where status = 3;
