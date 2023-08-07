da068176d63c946300d04dcf7ec1f814fa397995
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2023-08-07 10:47:21 -0700

    Update schema+loading scripts for bhm_rm_v1_3 table (#212)
    Co-authored-by: Tom Dwelly <dwelly@mpe.mpg.de>

---
+++ schema/sdss5db/catalogdb/bhm_rm/v1.3/bhm_rm_v1_3_create_table.sql
@@ -0,0 +1,27 @@
+-- Based on the information in the below SDSS wiki page
+-- https://wiki.sdss.org/display/BHM/Updates+to+BHM+RM+catalogs+and+cartons+in+v1.0+targeting
+
+create table catalogdb.bhm_rm_v1_3 (
+rm_field_name char(8) not null,
+ra double precision,
+dec double precision,
+catalogidv05 bigint,
+rm_known_spec boolean,
+rm_core boolean,
+rm_var boolean,
+rm_ancillary boolean,
+mag_g real,
+mag_r real,
+mag_i real,
+mag_z real,
+gaia_g real,
+gaia_bp real,
+gaia_rp real,
+ls_id_dr8 bigint,
+ls_id_dr10 bigint,
+gaia_dr2_source_id bigint,
+gaia_dr3_source_id bigint,
+panstarrs1_catid_objid bigint,
+rm_unsuitable boolean,
+rm_xrayqso integer
+);
---
+++ schema/sdss5db/catalogdb/bhm_rm/v1.3/bhm_rm_v1_3_load.sql
@@ -0,0 +1,3 @@
+\o bhm_rm_v1_3_load.out
+\copy catalogdb.bhm_rm_v1_3 from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/RM/v1.3/csv/bhm_rm_v1_3_31Jul2023.csv' delimiter ',' null 'NULL';
+\o
