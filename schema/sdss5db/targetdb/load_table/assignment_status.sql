ca977056b097f942f85eabbeea806c3fb0f2898a
John Donor <j.donor@tcu.edu>
2023-08-14 17:00:49 -0500

    assignment_status for lco

--- schema/sdss5db/targetdb/load_table/assignment_status.sql
+++ schema/sdss5db/targetdb/load_table/assignment_status.sql
@@ -16,8 +16,55 @@ join targetdb.design as des on asn.design_id = des.design_id
 join opsdb_apo.design_to_status as d2s on d2s.design_id = des.design_id
 where true ON CONFLICT DO NOTHING;
 
+insert into targetdb.assignment_status (pk, assignment_pk, status, mjd)
+select nextval('targetdb.assignment_status_pk_seq'), 
+asn.pk, d2s.completion_status_pk, d2s.mjd
+from targetdb.assignment as asn
+join targetdb.design as des on asn.design_id = des.design_id
+join opsdb_lco.design_to_status as d2s on d2s.design_id = des.design_id
+where true ON CONFLICT DO NOTHING;
+
 update targetdb.assignment_status set status = 0, mjd = null
 where status = 1 or status = 2;
 
 update targetdb.assignment_status set status = 1
 where status = 3;
+
+with boss_stat as (
+select stat.pk
+from targetdb.assignment_status as stat
+join targetdb.assignment as assn on assn.pk = stat.assignment_pk
+where stat.mjd < 60000 and assn.instrument_pk = 0
+)
+
+update targetdb.assignment_status assn_stat
+set status = 0, mjd = null
+from boss_stat
+where boss_stat.pk = assn_stat.pk;
+
+with ap_stat as (
+select stat.pk
+from targetdb.assignment_status as stat
+join targetdb.assignment as assn on assn.pk = stat.assignment_pk
+where stat.mjd < 59853 and assn.instrument_pk = 1
+)
+
+update targetdb.assignment_status assn_stat
+set status = 0, mjd = null
+from ap_stat
+where ap_stat.pk = assn_stat.pk;
+
+-- just an example below, to be used for incremental updates as needed
+
+with new_stat as (
+select stat.pk, d2s.mjd
+from targetdb.assignment_status as stat
+join targetdb.assignment as assn on assn.pk = stat.assignment_pk
+join opsdb_apo.design_to_status as d2s on d2s.design_id = assn.design_id 
+where d2s.mjd > 60076.0 and d2s.completion_status_pk = 3
+)
+
+update targetdb.assignment_status assn_stat
+set status = 1, mjd = new_stat.mjd
+from new_stat
+where new_stat.pk = assn_stat.pk;
