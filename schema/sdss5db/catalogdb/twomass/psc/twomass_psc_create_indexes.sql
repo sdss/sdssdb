7e1d5738a42ad02d657dc05b8e69fa75416f5939
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2023-09-08 15:30:44 -0700

    Twomass psc (#220)
    * add twomass_psc_load.py
    
    * add twomass_psc_load.py

---
+++ schema/sdss5db/catalogdb/twomass/psc/twomass_psc_create_indexes.sql
@@ -0,0 +1,14 @@
+
+CREATE INDEX ON catalogdb.twomass_psc (q3c_ang2ipix(ra, decl));
+
+CREATE UNIQUE INDEX ON catalogdb.twomass_psc(designation);
+
+CREATE INDEX ON catalogdb.twomass_psc (j_m);
+CREATE INDEX ON catalogdb.twomass_psc (h_m);
+CREATE INDEX ON catalogdb.twomass_psc (k_m);
+CREATE INDEX ON catalogdb.twomass_psc (ph_qual);
+CREATE INDEX ON catalogdb.twomass_psc (cc_flg);
+CREATE INDEX ON catalogdb.twomass_psc (gal_contam);
+CREATE INDEX ON catalogdb.twomass_psc (rd_flg);
+CREATE INDEX ON catalogdb.twomass_psc (jdate);
+
