3d044fc3ac495bdffa1d2f7128798e312c3f5f98
johndonor3 <j.donor@tcu.edu>
2023-09-28 14:47:33 -0600

    use version for assigned_targets view

--- schema/sdss5db/targetdb/load_table/assigned_targets/assigned_targets_view.sql
+++ schema/sdss5db/targetdb/load_table/assigned_targets/assigned_targets_view.sql
@@ -11,12 +11,13 @@ SELECT c.program, c.pk as carton_pk,
 FROM targetdb.assignment AS assn
 INNER JOIN targetdb.design AS d ON (assn.design_id = d.design_id)
 INNER JOIN targetdb.design_to_field AS d2f ON (d.design_id = d2f.design_id)
-INNER JOIN opsdb_apo.design_to_status AS d2s ON (d.design_id = d2s.design_id)
+INNER JOIN opsdb_lco.design_to_status AS d2s ON (d.design_id = d2s.design_id)
 INNER JOIN targetdb.field AS f ON (f.pk = d2f.field_pk)
 INNER JOIN targetdb.carton_to_target AS c2t ON (assn.carton_to_target_pk = c2t.pk)
 INNER JOIN targetdb.carton AS c ON (c2t.carton_pk = c.pk)
 INNER JOIN targetdb.observatory as o on (f.observatory_pk = o.pk)
 INNER JOIN targetdb.version as v on (f.version_pk = v.pk)
+WHERE v.plan = 'eta-5'
 WITH DATA;
 
 create unique index at_assn_idx on targetdb.assigned_targets (assignment_pk);
