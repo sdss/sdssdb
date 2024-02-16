574841d540acf05bedad3012177cd5182a62c012
Pramod Gupta <psgupta@astro.washington.edu>
2024-02-16 13:06:32 -0700

    marvels dr11, dr12: add *.sql files

---
+++ schema/sdss5db/catalogdb/marvels/dr12/marvels_dr12_velocitycurve_uf1d_load.sql
@@ -0,0 +1,3 @@
+\o marvels_dr12_velocitycurve_uf1d_load.out
+
+\copy catalogdb.marvels_dr12_velocitycurve_uf1d from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/marvels/dr12/dr12_marvelsVelocityCurveUF1D_ndelee.csv' csv header;
