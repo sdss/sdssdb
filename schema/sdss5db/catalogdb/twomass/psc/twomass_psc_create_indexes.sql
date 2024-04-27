8737a64eb152108955bed86c41d73047c3a67b32
Pramod Gupta <psgupta@astro.washington.edu>
2024-04-27 11:06:22 -0600

    edit twomass_psc_create_table.sql and twomass_psc_create_indexes.sql

--- schema/sdss5db/catalogdb/twomass/psc/twomass_psc_create_indexes.sql
+++ schema/sdss5db/catalogdb/twomass/psc/twomass_psc_create_indexes.sql
@@ -1,7 +1,10 @@
 
 CREATE INDEX ON catalogdb.twomass_psc (q3c_ang2ipix(ra, decl));
 
+-- Below both are done for historical reasons.
 CREATE UNIQUE INDEX ON catalogdb.twomass_psc(designation);
+ALTER TABLE catalogdb.twomass_psc
+    ADD CONSTRAINT twomass_psc_designation_uniq UNIQUE (designation);
 
 CREATE INDEX ON catalogdb.twomass_psc (j_m);
 CREATE INDEX ON catalogdb.twomass_psc (h_m);
