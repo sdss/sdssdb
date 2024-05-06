9c8bed76fc61fdf7efde2ff36b8a2e4a476a397a
Pramod Gupta <psgupta@astro.washington.edu>
2024-05-06 14:22:42 -0600

    edit sdss_dr17_specobj_add_columns.sql

--- schema/sdss5db/catalogdb/sdssSpecObj/dr17/sdss_dr17_specobj_add_columns.sql
+++ schema/sdss5db/catalogdb/sdssSpecObj/dr17/sdss_dr17_specobj_add_columns.sql
@@ -15,9 +15,23 @@
 --
 --
 
+-- We use the below "SQL update" command to ensure that
+-- only those objid values which
+-- exist in the table sdss_dr13_photoobj_primary are put in 
+-- the column bestobjid_bigint of the table sdss_dr17_specobj.
+-- This allows creating a foreign key from sdss_dr17_specobj(bestobjid_bigint)
+-- to sdss_dr13_photoobj(objid). 
+-- Note that the foreign key is to sdss_dr13_photoobj
+-- and to not sdss_dr13_photoobj_primary. This is because
+-- sdss_dr13_photoobj_primary is a materialized view and cannot be used
+-- for foreign keys.
 
 alter table catalogdb.sdss_dr17_specobj add column bestobjid_bigint bigint;
-update catalogdb.sdss_dr17_specobj set bestobjid_bigint = bestobjid::bigint;
+update catalogdb.sdss_dr17_specobj s set (bestobjid_bigint) = (select p.objid from sdss_dr13_photoobj_primary p where p.objid=s.bestobjid::bigint);
 
+-- sdss_dr13_photoobj_primary and sdss_dr13_photoobj
+-- do not have a fluxobjid column.
+-- So we use below "SQL update" instead of "SQL update" like above.
 alter table catalogdb.sdss_dr17_specobj add column fluxobjid_bigint bigint;
 update catalogdb.sdss_dr17_specobj set fluxobjid_bigint = fluxobjid::bigint;
+
