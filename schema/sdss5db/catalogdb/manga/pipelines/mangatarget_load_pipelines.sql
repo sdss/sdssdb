6e95144cab229e66acec123225c6b7ff36cf05c4
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2024-05-15 12:18:23 -0700

    mangatarget (#249)
    * add *.sql for mangatarget on pipelines server
    
    * update *.sql for mangatarget on pipelines server

---
+++ schema/sdss5db/catalogdb/manga/pipelines/mangatarget_load_pipelines.sql
@@ -0,0 +1,3 @@
+\o mangatarget_load_pipelines.out
+
+\copy catalogdb.mangatarget from '/uufs/chpc.utah.edu/common/home/sdss10/sdss5/target/catalogs/manga/drp/sqlMangaTarget-v1_2_27.csv_0001.with_specobjid.csv' csv header;
