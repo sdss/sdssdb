dd1eadb4d6800b3caf0a606cfdd4dd0645d63c38
José Sánchez-Gallego <gallegoj@uw.edu>
2024-07-02 12:58:57 -0700

    Merge targeting_generation_v2 (#254)
    * Extend structure+content of targeting_generation_to_version - intended to replace minidb.drXX_carton_txt
    
    * fix typo in README
    
    * Add rs_stage and rs_active to model classes
    
    * Fix issues with load_targeting_generation.sql
    
    ---------
    
    Co-authored-by: Tom Dwelly <dwelly@mpe.mpg.de>

--- schema/sdss5db/targetdb/load_table/targeting_generation/load_targeting_generation.sql
+++ schema/sdss5db/targetdb/load_table/targeting_generation/load_targeting_generation.sql
@@ -7,30 +7,20 @@ CREATE TEMPORARY TABLE IF NOT EXISTS targeting_generation_temp (
 
 \copy targeting_generation_temp FROM 'targeting_generation.csv' WITH CSV HEADER;
 
-CREATE TABLE IF NOT EXISTS targetdb.targeting_generation (
-    pk INTEGER,
-    label TEXT,
-    first_release TEXT
-);
-
 INSERT INTO targetdb.targeting_generation (pk, label, first_release)
     SELECT * FROM targeting_generation_temp ON CONFLICT DO NOTHING;
 
 
-
 CREATE TEMPORARY TABLE IF NOT EXISTS targeting_generation_to_carton_temp (
     generation_pk INTEGER,
-    carton_pk INTEGER
+    carton_pk INTEGER,
+    rs_stage TEXT,
+    rs_active BOOLEAN
 );
 
 \copy targeting_generation_to_carton_temp FROM 'targeting_generation_to_carton.csv' WITH CSV HEADER;
 
-CREATE TABLE IF NOT EXISTS targetdb.targeting_generation_to_carton (
-    generation_pk INTEGER,
-    carton_pk INTEGER
-);
-
-INSERT INTO targetdb.targeting_generation_to_carton (generation_pk, carton_pk)
+INSERT INTO targetdb.targeting_generation_to_carton (generation_pk, carton_pk, rs_stage, rs_active)
     SELECT * FROM targeting_generation_to_carton_temp ON CONFLICT DO NOTHING;
 
 
@@ -40,11 +30,6 @@ CREATE TEMPORARY TABLE IF NOT EXISTS targeting_generation_to_version_temp (
 );
 
 \copy targeting_generation_to_version_temp FROM 'targeting_generation_to_version.csv' WITH CSV HEADER;
-
-CREATE TABLE IF NOT EXISTS targetdb.targeting_generation_to_version (
-    generation_pk INTEGER,
-    version_pk INTEGER
-);
 
 INSERT INTO targetdb.targeting_generation_to_version (generation_pk, version_pk)
     SELECT * FROM targeting_generation_to_version_temp ON CONFLICT DO NOTHING;
