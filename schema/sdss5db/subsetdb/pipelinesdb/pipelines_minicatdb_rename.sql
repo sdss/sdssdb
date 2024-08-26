dc2f6b8ae1a19c5e03a3b44aac5e6d8211fc6da9
Pramod Gupta <psgupta@astro.washington.edu>
2024-08-26 16:58:08 -0600

    subsetdb:pipelinesdb:add *sql files

--- schema/sdss5db/subsetdb/pipelinesdb/pipelines_minicatdb_rename.sql
+++ schema/sdss5db/subsetdb/pipelinesdb/pipelines_minicatdb_rename.sql
@@ -1,4 +1,25 @@
 -- run this on pipelines only
+-- run this at a time which is fine for the pipelines team
+-- use sed to change date 2024aug17 appropriately
+
+-- rename the old catalogdb tables
+alter table catalogdb.allwise rename to allwise_2024aug17;
+alter table catalogdb.catalog rename to catalog_2024aug17;
+alter table catalogdb.catwise rename to catwise_2024aug17;
+alter table catalogdb.catwise2020 rename to catwise2020_2024aug17;
+alter table catalogdb.gaia_dr2_source rename to gaia_dr2_source_2024aug17;
+alter table catalogdb.gaia_dr3_astrophysical_parameters rename to gaia_dr3_astrophysical_parameters_2024aug17;
+alter table catalogdb.gaia_dr3_source rename to gaia_dr3_source_2024aug17;
+alter table catalogdb.legacy_survey_dr10 rename to legacy_survey_dr10_2024aug17;
+alter table catalogdb.legacy_survey_dr8 rename to legacy_survey_dr18_2024aug17;
+alter table catalogdb.panstarrs1 rename to panstarrs1_2024aug17;
+alter table catalogdb.sdss_dr13_photoobj rename to sdss_dr13_photoobj_2024aug17;
+alter table catalogdb.sdss_dr13_photoobj_primary rename to sdss_dr13_photoobj_primary_2024aug17;
+alter table catalogdb.tic_v8 rename to tic_v8_2024aug17;
+alter table catalogdb.supercosmos rename to supercosmos_2024aug17;
+alter table catalogdb.unwise rename to unwise_2024aug17;
+
+-- set schema of the new minicatdb tables to catalogdb 
 alter table minicatdb.allwise set schema catalogdb;
 alter table minicatdb.catalog set schema catalogdb;
 alter table minicatdb.catwise set schema catalogdb;
