7bf386dc618050fea4354b01519a32c4c8e97562
Pramod Gupta <psgupta@astro.washington.edu>
2024-08-26 18:11:25 -0600

    subsetdb/pipelinesdb/pipedb: add *sql files

--- schema/sdss5db/subsetdb/pipelinesdb/pipedb/sdss_dr13_photoobj_primary_full_copyfrom.sql
+++ schema/sdss5db/subsetdb/pipelinesdb/pipedb/sdss_dr13_photoobj_primary_full_copyfrom.sql
@@ -1,4 +1,4 @@
 -- run this on pipelines only
 
-\copy catalogdb.sdss_dr13_photoobj_primary from to '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/minicatdb/sdss_dr13_photoobj_primary_catalogdb_full.csv' delimiter ';' csv header;
+\copy catalogdb.sdss_dr13_photoobj_primary from '/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/target/catalogs/minicatdb/sdss_dr13_photoobj_primary_catalogdb_full.csv' delimiter ';' csv header;
 
