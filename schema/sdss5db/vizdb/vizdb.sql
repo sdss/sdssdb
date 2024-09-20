82d124e270fba4de98674b3f6525a85620bea8ae
havok2063 <havok2063@hotmail.com>
2024-09-20 18:19:07 -0400

    updating vizdb metadata and sql for db changes

--- schema/sdss5db/vizdb/vizdb.sql
+++ schema/sdss5db/vizdb/vizdb.sql
@@ -39,23 +39,22 @@ CREATE MATERIALIZED VIEW vizdb.sdssid_to_pipes AS
 SELECT row_number() over(order by s.sdss_id) as pk, s.sdss_id,
        (b.sdss_id IS NOT NULL) AS in_boss,
        (v.star_pk IS NOT NULL) AS in_apogee,
+	   (o.source_pk IS NOT NULL) AS in_bvs,
        (a.sdss_id IS NOT NULL) AS in_astra,
-       (b.sdss_id IS NOT NULL OR v.star_pk IS NOT NULL OR a.sdss_id IS NOT NULL) AS has_been_observed
-    --    b.id as boss_spectrum_pk,
-    --    v.star_pk as apogee_star_pk,
-    --    v.visit_pk as apogee_visit_pk,
-    --    a.pk as astra_source_pk,
-    --    v.pk as astra_apovisit_spec_pk,
-    --    o.pk as astra_bossvisit_spec_pk
+       (b.sdss_id IS NOT NULL OR v.star_pk IS NOT NULL OR a.sdss_id IS NOT NULL) AS has_been_observed,
+       case when b.sdss_id IS NOT NULL then 'sdss5' when v.source_pk IS NOT NULL then v.release when o.source_pk is NOT NULL then o.release end as release,
+       case when b.sdss_id IS NOT NULL then lower(b.obs) when v.source_pk IS NOT NULL then substring(v.telescope,0,4) when o.source_pk IS NOT NULL then substring(o.telescope,0,4) end as obs,
+       case when b.sdss_id IS NOT NULL then b.mjd when v.source_pk IS NOT NULL then v.mjd when o.source_pk IS NOT NULL then o.mjd end as mjd
 FROM vizdb.sdss_id_stacked AS s
 LEFT JOIN boss_drp.boss_spectrum AS b ON s.sdss_id = b.sdss_id
 LEFT JOIN astra_050.source AS a ON s.sdss_id = a.sdss_id
 LEFT JOIN astra_050.apogee_visit_spectrum as v on v.source_pk=a.pk
---LEFT JOIN astra_050.boss_visit_spectrum as o on o.source_pk=a.pk
+LEFT JOIN astra_050.boss_visit_spectrum as o on o.source_pk=a.pk
 WITH DATA;
 
 CREATE UNIQUE INDEX CONCURRENTLY ON vizdb.sdssid_to_pipes USING BTREE(pk);
 CREATE INDEX CONCURRENTLY ON vizdb.sdssid_to_pipes USING BTREE(sdss_id);
+CREATE INDEX CONCURRENTLY ON vizdb.sdssid_to_pipes USING BTREE(mjd);
 
 
 -- Refresh the views with the following commands:
@@ -103,4 +102,6 @@ CREATE INDEX CONCURRENTLY ON vizdb.releases USING BTREE(release);
 
 -- GRANT permissions
 GRANT USAGE ON SCHEMA vizdb TO sdss;
+GRANT SELECT ON vizdb.sdss_id_stacked TO sdss;
+GRANT SELECT ON vizdb.sdss_id_flat TO sdss;
 GRANT SELECT ON vizdb.sdssid_to_pipes TO sdss;
