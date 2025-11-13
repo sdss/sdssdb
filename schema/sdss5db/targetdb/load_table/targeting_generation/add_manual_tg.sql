8e648e225b154a8fb8c6b68a75c9a43f12f3e3e8
Tom Dwelly <dwelly@mpe.mpg.de>
2025-11-13 09:39:42 +0000

    Added some manual fps position cartons to targeting_generation='dr20.manual'

--- schema/sdss5db/targetdb/load_table/targeting_generation/add_manual_tg.sql
+++ schema/sdss5db/targetdb/load_table/targeting_generation/add_manual_tg.sql
@@ -6,7 +6,6 @@ e.g. in manual FPS designs (RM fields, commissioning/calibration
 designs etc).
 */
 
-
 /* This is where to set the name of the new targeting_generation */
 \set tg_label 'dr20.manual'
  
@@ -14,7 +13,7 @@ INSERT INTO targetdb.targeting_generation (label, first_release)
 VALUES (:'tg_label', 'dr20') ON CONFLICT DO NOTHING; 
 
 
-/* Prepare a temp table to hold teh tg to carton asscoiations */
+/* Prepare a temp table to hold the targeting_generation_to_carton associations */
 CREATE TEMPORARY TABLE IF NOT EXISTS targeting_generation_to_carton_temp (
     generation_label TEXT,
     carton_pk INTEGER,
@@ -25,12 +24,16 @@ TRUNCATE targeting_generation_to_carton_temp;
 
 /* add your list of manual carton pks below */
 INSERT INTO targeting_generation_to_carton_temp (generation_label, carton_pk, rs_stage, rs_active)
-    VALUES                             /* carton.carton                version.plan */
-    (:'tg_label', 1165, 'none', 'f'),  /* manual_bhm_spiders_comm_lco        0.5.23 */
-    (:'tg_label', 1639, 'none', 'f'),  /* bhm_rm_core                        1.0.48 */
-    (:'tg_label', 1640, 'none', 'f'),  /* bhm_rm_known_spec                  1.0.48 */
-    (:'tg_label', 1641, 'none', 'f'),  /* bhm_rm_var                         1.0.48 */
-    (:'tg_label', 1642, 'none', 'f')   /* bhm_rm_ancillary                   1.0.48 */
+    VALUES                             /* carton.carton                           version.plan */
+    (:'tg_label', 1105, 'none', 'f'),  /* manual_fps_position_stars_10            0.5.3        */
+    (:'tg_label', 1135, 'none', 'f'),  /* manual_fps_position_stars_apogee_10     0.5.13       */
+    (:'tg_label', 1158, 'none', 'f'),  /* manual_bright_target_offsets_3          0.5.20       */
+    (:'tg_label', 1165, 'none', 'f'),  /* manual_bhm_spiders_comm_lco             0.5.23       */
+    (:'tg_label', 1166, 'none', 'f'),  /* manual_fps_position_stars_lco_apogee_10 0.5.23       */
+    (:'tg_label', 1639, 'none', 'f'),  /* bhm_rm_core                             1.0.48       */
+    (:'tg_label', 1640, 'none', 'f'),  /* bhm_rm_known_spec                       1.0.48       */
+    (:'tg_label', 1641, 'none', 'f'),  /* bhm_rm_var                              1.0.48       */
+    (:'tg_label', 1642, 'none', 'f')   /* bhm_rm_ancillary                        1.0.48       */
     ON CONFLICT DO NOTHING;
 
 
@@ -43,7 +46,8 @@ INSERT INTO targetdb.targeting_generation_to_carton (generation_pk, carton_pk, r
     
 
 /* Now associate the new targeting generation with 
-the 'manual' entry in targetdb.version */
+the 'manual' entry in targetdb.version (version.pk=84) */
+\set version_pk_manual 84
 
 CREATE TEMPORARY TABLE IF NOT EXISTS targeting_generation_to_version_temp (
     generation_label TEXT,
@@ -52,7 +56,7 @@ CREATE TEMPORARY TABLE IF NOT EXISTS targeting_generation_to_version_temp (
 TRUNCATE targeting_generation_to_version_temp;
 
 INSERT INTO targeting_generation_to_version_temp(generation_label, version_pk)
-    VALUES (:'tg_label', 84 ) 
+    VALUES (:'tg_label', :'version_pk_manual' ) 
 ON CONFLICT DO NOTHING; 
 
 INSERT INTO targetdb.targeting_generation_to_version (generation_pk, version_pk)
@@ -64,7 +68,6 @@ INSERT INTO targetdb.targeting_generation_to_version (generation_pk, version_pk)
     
 
 /* test that the new cartons have been successfully added */
-
 
 SELECT tg.label,tgv.plan as rs_plan, c.pk as carton_pk, c.carton, cv.plan as carton_plan
 FROM targetdb.targeting_generation AS tg
@@ -79,5 +82,4 @@ ON tg2c.carton_pk = c.pk
 JOIN targetdb.version as cv
 ON c.version_pk = cv.pk
 WHERE tg.label = :'tg_label';
-
 
