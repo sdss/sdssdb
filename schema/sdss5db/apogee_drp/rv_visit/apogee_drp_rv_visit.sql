de7ced811a1467d8b2fbea8b2e37e7278d966cad
David Nidever <dnidever@montana.edu>
2026-05-25 12:16:29 -0600

    added STARFLAGS to apogee_drp_rv_visit.sql

--- schema/sdss5db/apogee_drp/rv_visit/apogee_drp_rv_visit.sql
+++ schema/sdss5db/apogee_drp/rv_visit/apogee_drp_rv_visit.sql
@@ -46,6 +46,7 @@ CREATE TABLE apogee_drp.rv_visit (
     V_APRED     text,  --/D Git hash string of apogee_drp software used
     GOODVISIT   bool, --/D Boolean flag indicating this visit was used in the combined spectrum and average RV
     STARFLAG	integer,  --/D Quality flags as integer
+    STARFLAGS	text,  --/D Quality flags as comma-delimited ASCII text
     CREATED timestamp with time zone DEFAULT now() NOT NULL,  --/D Timestamp the rv_visit record was created
     UNIQUE(APRED_VERS,TELESCOPE,PLATE,MJD,FIBERID,STARVER)
 );
